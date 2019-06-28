5e243f157fd78109f13919793c3359d5e1bc3f8c
stone725 <inf33554432@gmail.com>
2019-06-29 08:32:09 +0900

    https://atcoder.jp/contests/abs/tasks/abc081_b

---
+++ AtCoder/ABS/abc081_b.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    int ans = 64;
+    for(auto i : a){
+        int cnt = 0;
+        while(i % 2 == 0){
+            cnt++;
+            i /= 2;
+        }
+        ans = min(ans, cnt);
+    }
+    cout << ans << "\n";
+}
