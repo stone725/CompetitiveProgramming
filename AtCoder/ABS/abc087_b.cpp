fb9637b96bcfd61bc64fe3789fb51b88a7f47bfd
stone725 <inf33554432@gmail.com>
2019-06-29 13:17:18 +0900

    https://atcoder.jp/contests/abs/tasks/abc087_b

---
+++ AtCoder/ABS/abc087_b.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int a, b, c, x;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> x;
+    int ans = 0;
+    for(int i = min(x / 500, a); i >= 0; i--){
+        for(int j = min((x - i * 500) / 100, b); j >= 0; j--){
+            if((x - i * 500 - j * 100) / 50 <= c) ans++;
+            else break;
+        }
+    }
+    cout << ans++;
+}
