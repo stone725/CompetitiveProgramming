4a14fef69d9c5098add0b0b86f2716efab08c398
stone725 <inf33554432@gmail.com>
2019-06-29 19:24:46 +0900

    https://atcoder.jp/contests/abs/tasks/abc085_b

---
+++ AtCoder/ABS/abc085_b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    set<int> cnt;
+    for(int i = 0; i < n; i++){
+        int d;
+        cin >> d;
+        cnt.insert(d);
+    }
+    cout << cnt.size() << "\n";
+}
