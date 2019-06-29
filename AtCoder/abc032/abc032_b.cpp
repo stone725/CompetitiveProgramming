510d72ac5faff0c4801e67046292eb76ca3e775f
stone725 <inf33554432@gmail.com>
2019-06-30 08:57:59 +0900

    https://atcoder.jp/contests/abc032/tasks/abc032_b

---
+++ AtCoder/abc032/abc032_b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <set>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    int n;
+    set<string> s;
+    cin >> str;
+    cin >> n;
+    for(int i = 0; i + n <= str.size(); i++){
+        s.insert(str.substr(i, n));
+    }
+    cout << s.size() << "\n";
+}
