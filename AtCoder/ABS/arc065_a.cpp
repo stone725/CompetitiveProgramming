9e4430dae45f531c30dec4202b41208e63ac7354
stone725 <inf33554432@gmail.com>
2019-06-29 19:42:24 +0900

    https://atcoder.jp/contests/abs/tasks/arc065_a

---
+++ AtCoder/ABS/arc065_a.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    string addStrings[] = {"dream", "dreamer", "erase", "eraser"};
+    for(auto&& i : addStrings){
+        reverse(begin(i), end(i));
+    }
+    reverse(begin(s), end(s));
+    for(int i = 0; i < s.size(); ){
+        bool flag = true;
+        for(auto&& j : addStrings){
+            if(s.size() < i + j.size()) continue;
+            if(s.substr(i, j.size()) == j){
+                i += j.size();
+                flag = false;
+            }
+        }
+        if(flag){
+            cout << "NO\n";
+            return 0;
+        }
+    }
+    cout << "YES\n";
+}
