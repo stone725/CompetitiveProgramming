6f9af904d3c763568e634935e3568cd1336abc86
stone725 <inf33554432@gmail.com>
2019-06-30 19:13:10 +0900

    https://atcoder.jp/contests/abc039/tasks/abc039_c

---
+++ AtCoder/abc039/abc039_c.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str, checkStr = "WBWBWWBWBWBW";
+    cin >> str; 
+    int check = 0;
+    for(int i = 0; i < 7; i++){
+        if(str.substr(0, checkStr.size()) == checkStr){
+            check = i;
+            break;
+        }
+        do{
+            checkStr = checkStr.substr(1) + checkStr[0];
+        }while(checkStr[0] != 'W');
+    }
+    switch(check){
+        case 0:
+            cout << "Do\n";
+            break;
+        case 1:
+            cout << "Re\n";
+            break;
+        case 2:
+            cout << "Mi\n";
+            break;
+        case 3:
+            cout << "Fa\n";
+            break;
+        case 4:
+            cout << "So\n";
+            break;
+        case 5:
+            cout << "La\n";
+            break;
+        case 6:
+            cout << "Si\n";
+            break;
+    }
+}
