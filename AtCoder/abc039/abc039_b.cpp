bdfeea373cf3e9f02e4d8cc8e2786b4a77b8b891
stone725 <inf33554432@gmail.com>
2019-06-30 18:50:42 +0900

    https://atcoder.jp/contests/abc039/tasks/abc039_b

---
+++ AtCoder/abc039/abc039_b.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int ans;
+    for(ans = 1; ans * ans * ans * ans < n; ans++);
+    cout << ans << "\n";
+}
