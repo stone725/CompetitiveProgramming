8d7d23a11a3ebe7bfa310f1431f2c43828538a37
stone725 <inf33554432@gmail.com>
2019-06-30 10:06:09 +0900

    https://atcoder.jp/contests/abc032/tasks/abc032_c

---
+++ AtCoder/abc032/abc032_c.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    
+    vector<int> s(n);
+    for(auto&& i : s){
+        cin >> i;
+        if(i == 0){
+            cout << n << "\n";
+            return 0;
+        }
+    }
+    if(k == 0){
+        cout << 0 << "\n";
+        return 0;
+    }
+    int st = 0, go = 1, ans = 0;
+    unsigned long long now = 1;
+    while(go <= n && st <= go){
+        now *= s[go - 1];
+        while(now > k && st < go){
+            now /= s[st];
+            st++;
+        }
+        ans = max(ans, go - st);
+        go++;
+    }
+
+    cout << ans << "\n";
+}
