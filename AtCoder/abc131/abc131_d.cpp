fb3144feb04da9e84d599ea6aab41fc2a7bd52a7
stone725 <inf33554432@gmail.com>
2019-06-30 23:59:50 +0900

    https://atcoder.jp/contests/abc131/tasks/abc131_d

---
+++ AtCoder/abc131/abc131_d.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<pair<int, int>> w(n);
+    for(auto&& i : w){
+        cin >> i.second >> i.first;
+    }
+    sort(begin(w), end(w));
+    long long now = 0;
+    for(auto&& i : w){
+        now += i.second;
+        if(i.first < now){
+            cout << "No\n";
+            return 0;
+        }
+    }
+    cout << "Yes\n";
+}
