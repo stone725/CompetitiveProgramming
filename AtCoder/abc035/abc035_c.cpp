8a5d6e7ab218df686dc90a71775875a4f5026db8
stone725 <inf33554432@gmail.com>
2019-07-01 19:58:30 +0900

    https://atcoder.jp/contests/abc035/tasks/abc035_c

---
+++ AtCoder/abc035/abc035_c.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n, q;
+    cin >> n >> q;
+    vector<int> bits(n + 1);
+    while(q--){
+        int l, r;
+        cin >> l >> r;
+        bits[l - 1]++;
+        bits[r]--;
+    }
+    cout << bits[0] % 2;
+    for(int i = 1; i < n; i++){
+        bits[i] += bits[i - 1];
+        cout << bits[i] % 2;
+    }
+    cout << "\n";
+}
