408789c084e20e0beb791bbec8c25bb7804c683d
stone725 <inf33554432@gmail.com>
2019-07-01 12:48:23 +0900

    https://atcoder.jp/contests/abc035/tasks/abc035_a

---
+++ AtCoder/abc035/abc035_a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int w, h;
+    cin >> w >> h;
+    if(w * 3 == h * 4){
+        cout << "4:3\n";
+    }else{
+        cout << "16:9\n";
+    }
+}
