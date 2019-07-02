cdf8a1b22f12178a906f5fed1d58324544866ac6
stone725 <inf33554432@gmail.com>
2019-07-02 21:39:44 +0900

    https://atcoder.jp/contests/abc037/tasks/abc037_b

---
+++ AtCoder/abc037/abc037_b.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for(int i = 0; i < q; i++){
+        int l, r, t;
+        cin >> l >> r >> t;
+        for(int i = l - 1; i < r; i++){
+            a[i] = t;
+        }
+    }
+    for(auto&& i : a){
+        cout << i << "\n";
+    }
+}
