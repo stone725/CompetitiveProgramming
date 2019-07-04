22a54f3b065d34072dc7158a0a32b00752f74cef
stone725 <inf33554432@gmail.com>
2019-07-04 14:44:24 +0900

    https://atcoder.jp/contests/abc038/tasks/abc038_a

---
+++ AtCoder/abc038/abc038_a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    if(str.back() == 'T'){
+        cout << "YES\n";
+    }else{
+        cout << "NO\n";
+    }
+}
