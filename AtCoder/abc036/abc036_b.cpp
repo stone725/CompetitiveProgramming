08d1806b266848eda4142805a12699b9fa6bb4b3
stone725 <inf33554432@gmail.com>
2019-07-04 14:17:28 +0900

    https://atcoder.jp/contests/abc036/tasks/abc036_b

---
+++ AtCoder/abc036/abc036_b.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<string> strs(n);
+    for(auto&& i : strs){
+        cin >> i;
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = n - 1; j >= 0; j--){
+            cout << strs[j][i];
+        }
+        cout << "\n";
+    }
+}
