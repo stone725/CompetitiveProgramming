9bdd03edbad0b5309d9a31dbc11ab3d4b9907f42
stone725 <inf33554432@gmail.com>
2019-07-06 18:48:05 +0900

    https://atcoder.jp/contests/abc041/tasks/abc041_b

---
+++ AtCoder/abc041/abc041_b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    const int INF = 1e9 + 7;
+    long long a, b, c;
+    cin >> a >> b >> c;
+    long long ans = a * b % INF;
+    ans *= c;
+    ans %= INF;
+    cout << ans << "\n";
+}
