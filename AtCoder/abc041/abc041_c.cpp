4d2eb501086916a580b376ca4a4766ca73fd46a7
stone725 <inf33554432@gmail.com>
2019-07-06 18:53:49 +0900

    https://atcoder.jp/contests/abc041/tasks/abc041_c

---
+++ AtCoder/abc041/abc041_c.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    vector<pair<int, int>> a(n);
+    for(int i = 0;  i < n; i++){
+        cin >> a[i].first;
+        a[i].second = i + 1;
+    }
+    sort(rbegin(a), rend(a));
+    for(auto&& i : a){
+        cout << i.second << "\n";
+    }
+}
