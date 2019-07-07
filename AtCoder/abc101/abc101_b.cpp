67e500a149e09b2d9f81b6b22ab52f9ae155c098
stone725 <inf33554432@gmail.com>
2019-07-07 20:41:37 +0900

    https://atcoder.jp/contests/abc101/tasks/abc101_b

---
+++ AtCoder/abc101/abc101_b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    int num = stoi(str), div = 0;
+    for(auto&& i : str){
+        div += i - '0';
+    }
+    if(num % div){
+        cout << "No\n";
+    }else{
+        cout << "Yes\n";
+    }
+}
