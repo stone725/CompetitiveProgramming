019cf9cc27be896409db8b7648fa0edc94362cc5
stone725 <inf33554432@gmail.com>
2019-07-08 19:53:03 +0900

    https://atcoder.jp/contests/abc102/tasks/abc102_b

---
+++ AtCoder/abc102/abc102_b.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            ans = max(ans, abs(a[i] - a[j]));
+        }
+    }
+    cout << ans << "\n";
+}
