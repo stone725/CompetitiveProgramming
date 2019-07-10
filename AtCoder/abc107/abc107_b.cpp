deef8d0e27114af1ce5193a970f30d60c82bff4e
stone725 <inf33554432@gmail.com>
2019-07-10 14:08:06 +0900

    https://atcoder.jp/contests/abc107/tasks/abc107_b

---
+++ AtCoder/abc107/abc107_b.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<bool> skipRow(n, true), skipColumn(m, true);
+    vector<string> vs(n);
+    for(int i = 0; i < n; i++){
+        cin >> vs[i];
+        for(int j = 0; j < m; j++){
+            if(vs[i][j] == '#'){
+                skipRow[i] = skipColumn[j] = false;
+            }
+        }
+    }
+    for(int i = 0; i < n; i++){
+        if(skipRow[i]) continue;
+        for(int j = 0; j < m; j++){
+            if(skipColumn[j]) continue;
+            cout << vs[i][j];
+        }
+        cout << "\n";
+    }
+}
