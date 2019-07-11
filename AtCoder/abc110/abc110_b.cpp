775e28ffc4a024e896beeb1f00b1fdde41156fb1
stone725 <inf33554432@gmail.com>
2019-07-11 12:58:15 +0900

    https://atcoder.jp/contests/abc110/tasks/abc110_b

---
+++ AtCoder/abc110/abc110_b.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, m, X, Y;
+    cin >> n >> m >> X >> Y;
+    vector<int> x(n), y(m);
+    for(auto&& i : x){
+        cin >> i;
+    }
+    for(auto&& i : y){
+        cin >> i;
+    }
+    sort(rbegin(x), rend(x));
+    sort(begin(y), end(y));
+    if(max(X, x[0]) < min(Y, y[0])){
+        cout << "No War\n";
+    }else{
+        cout << "War\n";
+    }
+}
