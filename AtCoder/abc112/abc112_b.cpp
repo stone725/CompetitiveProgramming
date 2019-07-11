0cc568d2dcdc538305f7562908340f1814c7ed6c
stone725 <inf33554432@gmail.com>
2019-07-12 08:10:21 +0900

    https://atcoder.jp/contests/abc112/tasks/abc112_b

---
+++ AtCoder/abc112/abc112_b.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, t;
+    cin >> n >> t;
+    int ans = 1001;
+    for(int i = 0; i < n; i++){
+        int c, ti;
+        cin >> c >> ti;
+        if(ti <= t){
+            ans = min(ans, c);
+        }
+    }
+    if(ans == 1001){
+        cout << "TLE\n";
+        return 0;
+    }
+    cout << ans << "\n";
+}
