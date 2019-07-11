62eb77bffe5b3a2d98ac075cc9a48f5c542c6c04
stone725 <inf33554432@gmail.com>
2019-07-11 13:50:01 +0900

    https://atcoder.jp/contests/abc111/tasks/abc111_b

---
+++ AtCoder/abc111/abc111_b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = n; i <= 999; i++){
+        if(i % 10 == i / 100 && (i / 10) % 10 == i % 10){
+            cout << i << "\n";
+            return 0;
+        }
+    }
+}
