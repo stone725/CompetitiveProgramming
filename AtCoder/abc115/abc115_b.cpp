5703ade78f2cd2f1721b6efdb2b6c73d73f1cdfc
stone725 <inf33554432@gmail.com>
2019-07-13 02:42:14 +0900

    https://atcoder.jp/contests/abc115/tasks/abc115_b

---
+++ AtCoder/abc115/abc115_b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    for(auto&& i : p){
+        cin >> i;
+    }
+    sort(rbegin(p), rend(p));
+    p[0] /= 2;
+    cout << accumulate(begin(p), end(p), 0) << "\n";
+}
