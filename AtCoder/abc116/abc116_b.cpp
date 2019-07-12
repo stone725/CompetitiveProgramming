75a19fbbbc2ddbf67dc6d069cfac9298d8a1f1fa
stone725 <inf33554432@gmail.com>
2019-07-13 03:19:05 +0900

    https://atcoder.jp/contests/abc116/tasks/abc116_b

---
+++ AtCoder/abc116/abc116_b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int s;
+    cin >> s;
+    int now = s;
+    bool used[1000000] = {};
+    for(int i = 0; i < 1e6; i++){
+        if(used[now]){
+            cout << i + 1 << "\n";
+            return 0;
+        }
+        used[now] = true;
+        if(now % 2){
+            now *= 3;
+            now++;
+        }else{
+            now /= 2;
+        }
+    }
+}
