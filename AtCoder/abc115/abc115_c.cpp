e198a260c411337961e5e08b5bd6844bb449d099
stone725 <inf33554432@gmail.com>
2019-07-13 02:49:32 +0900

    https://atcoder.jp/contests/abc115/tasks/abc115_c

---
+++ AtCoder/abc115/abc115_c.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using  namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+    vector<int> h(n);
+    for(auto&& i : h){
+        cin >> i;
+    }
+    sort(begin(h), end(h));
+    int ans = 1e9;
+    for(int i = 0; i + k <= n; i++){
+        ans = min(ans, h[i + k - 1] - h[i]);
+    }
+    cout << ans << "\n";
+}
