ce484e9c0b2e560521fd0567b5991f1446be7fb1
stone725 <inf33554432@gmail.com>
2019-07-13 08:32:23 +0900

    https://atcoder.jp/contests/abc117/tasks/abc117_b

---
+++ AtCoder/abc117/abc117_b.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    sort(rbegin(a), rend(a));
+    if(a[0] < accumulate(begin(a) + 1, end(a), 0)){
+        cout << "Yes\n";
+    }else{
+        cout << "No\n";
+    }
+}
