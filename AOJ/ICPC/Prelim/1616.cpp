5fd8bf30688e99c2d5a49228b8fef5209502628b
stone725 <inf33554432@gmail.com>
2019-07-13 09:57:32 +0900

    AOJ1616

---
+++ AOJ/ICPC/Prelim/1616.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m;
+    vector<int> a;
+    while(cin >> n >> m && n && m){
+        a.resize(n);
+        for(auto&& i : a){
+            cin >> i;
+        }
+        sort(begin(a), end(a));
+        int ans = -1;
+        for(int i = 0; i + 1 < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(a[i] + a[j] > m) break;
+                ans = max(ans, a[i] + a[j]);
+            }
+        }
+        if(~ans){
+            cout << ans << "\n";
+        }else{
+            cout << "NONE\n";
+        }
+    }
+}
