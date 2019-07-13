6ccfce83bbd0c466d3f246b423b46f28fbc4763c
stone725 <inf33554432@gmail.com>
2019-07-13 09:40:59 +0900

    https://atcoder.jp/contests/abc120/tasks/abc120_b

---
+++ AtCoder/abc120/abc120_b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b){
+    return b ? gcd(b, a % b) : a;
+}
+
+int main(){
+    int a, b, k;
+    cin >> a >> b >> k;
+    int score = gcd(a, b);
+    int cnt = 0;
+    for(int i = score; i > 0; i--){
+        if(score % i == 0){
+            cnt++;
+            if(cnt == k){
+                cout << i << "\n";
+                return 0;
+            }
+        }
+    }
+}
