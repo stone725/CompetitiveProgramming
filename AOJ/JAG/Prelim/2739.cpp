0eefb45132584b78211da66d73cc1f0ded285c98
stone725 <inf33554432@gmail.com>
2019-07-13 22:45:45 +0900

    AOJ2739

---
+++ AOJ/JAG/Prelim/2739.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, m, t;
+    cin >> n >> m >> t;
+    vector<int> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    a.push_back(t + m);
+    int now = 0, ans = 0;
+    for(int i = 0; i < a.size(); i++){
+        ans += max(0, a[i] - now - m);
+        now = a[i] + m;
+    }
+    cout << ans << "\n";
+}
