109bef808cd08e2130a2fb2d8981f5b744d1ae1f
stone725 <inf33554432@gmail.com>
2019-07-13 10:13:00 +0900

    AOJ2881

---
+++ AOJ/JAG/Prelim/2881.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string str;
+    while(cin >> str && str != "#"){
+        int y, m, d;
+        cin >> y >> m >> d;
+        if(y < 31){
+            cout << str << " " << y << " " << m << " " << d << "\n";
+        } else if(y == 31){
+            if(m <= 4){
+                cout << str << " " << y << " " << m << " " << d << "\n";
+            }else{
+                cout << "? " << y - 30 << " " << m << " " << d << "\n";
+            }
+        } else{
+            cout << "? " << y - 30 << " " << m << " " << d << "\n";
+        }
+    }
+}
