b19e739dbc6b3d5f1e72055d602349cd79d1404d
stone725 <inf33554432@gmail.com>
2019-07-14 22:18:40 +0900

    https://atcoder.jp/contests/abc008/tasks/abc008_3

---
+++ AtCoder/abc008/abc008_3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> c(n);
+    for(auto&& i : c){
+        cin >> i;
+    }
+    sort(begin(c), end(c));
+    double ans = 0;
+    for(int i = 0; i < n; i++){
+        int cnt = 0;
+        for(int j = 0; j < n; j++){
+            if(c[i] % c[j] == 0) cnt++;
+        }
+        ans += 1. * (cnt / 2 + cnt % 2) / cnt;
+    }
+    cout << fixed << setprecision(10) << ans << "\n";
+}
