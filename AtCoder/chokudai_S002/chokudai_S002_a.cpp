1036c165761fab5441482cc9069e9e9dfe661366
stone725 <inf33554432@gmail.com>
2019-07-15 15:09:42 +0900

    chokudai_S002_a

---
+++ AtCoder/chokudai_S002/chokudai_S002_a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        unsigned long long a, b;
+        cin >> a >> b;
+        cout << a * b << "\n";
+    }
+}
