e4fbc2809796ac40372dc09b41327e30140d49ec
stone725 <inf33554432@gmail.com>
2019-07-15 15:44:12 +0900

    https://atcoder.jp/contests/agc002/tasks/agc002_a

---
+++ AtCoder/agc002/agc002_a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    if(a <= 0 && 0 <= b){
+        cout << "Zero\n";
+    }
+    else if(b < 0){
+        if((b - a) % 2){
+            cout << "Positive\n";
+        }else{
+            cout << "Negative\n";
+        }
+    }else{
+        cout << "Positive\n";
+    }
+}
