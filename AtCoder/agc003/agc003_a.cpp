90dd64699d06099c285b7b9156c630b0016ad5b5
stone725 <inf33554432@gmail.com>
2019-07-16 09:01:56 +0900

    https://atcoder.jp/contests/agc003/tasks/agc003_a

---
+++ AtCoder/agc003/agc003_a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    if(!!count(begin(str), end(str), 'N') == !!count(begin(str), end(str), 'S')){
+        if(!!count(begin(str), end(str), 'W') == !!count(begin(str), end(str), 'E')){
+            cout << "Yes\n";
+            return 0;
+        }
+    }
+    cout << "No\n";
+}
