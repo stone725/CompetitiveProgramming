54ecf907684be661341d7e15d7393d5332bea229
stone725 <inf33554432@gmail.com>
2019-07-18 08:58:31 +0900

    soundhound2018_b

---
+++ AtCoder/soundhound2018/soundhound2018_b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, l, r;
+    cin >> n >> l >> r;
+    int a;
+    cin >> a;
+    cout << min(max(l, a), r);
+    for(int i = 1; i < n; i++){
+        cin >> a;
+        cout << " " << min(max(l, a), r);
+    }
+    cout << "\n";
+}
