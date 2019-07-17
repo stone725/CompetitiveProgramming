31526acf440424f296035e272b726a4c064b07f8
stone725 <inf33554432@gmail.com>
2019-07-18 06:08:22 +0900

    nikkei2019_final_a

---
+++ AtCoder/nikkei2019_final/nikkei2019_final_a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<long long> a(n), b(n + 1);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    partial_sum(begin(a), end(a), begin(b) + 1);
+    for(int i = 1; i <= n; i++){
+        long long ans = b[i];
+        for(int j = 1; j + i <= n; j++){
+            ans = max(ans, b[j + i] - b[j]);
+        }
+        cout << ans << "\n";
+    }
+}
