c355ee81d9f15d15d21e7c4653741e3cd54e3b32
stone725 <inf33554432@gmail.com>
2019-07-17 23:14:02 +0900

    https://atcoder.jp/contests/agc031/tasks/agc031_a

---
+++ AtCoder/agc031/agc031_a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int INF = 1e9 + 7;
+
+int main(){
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+    int cnt[26];
+    for(int i = 0; i < 26; i++){
+        cnt[i] = 1;
+    }
+    for(auto&& i : str){
+        cnt[i - 'a']++;
+    }
+    long long ans = 1;
+    for(auto&& i : cnt){
+        ans *= i;
+        ans %= INF;
+    }
+    cout << ans - 1 << "\n";
+}
