2b6f05e946db004f357cbc4d2e2815ea28d4cf5c
stone725 <inf33554432@gmail.com>
2019-07-18 11:00:38 +0900

    dwacon5th_prelims_a

---
+++ AtCoder/dwacon5th_prelims/dwacon5th_prelims_a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    double avg = accumulate(begin(a), end(a), 0.) / n;
+    int ans = 0;
+    for(int i = 1; i < n; i++){
+        if(abs(avg - a[ans]) > abs(avg - a[i])){
+            ans = i;
+        }
+    }
+    cout << ans << "\n";
+}
