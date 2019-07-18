824f95df23398bdfe68882aa63d8de25154809cd
stone725 <inf33554432@gmail.com>
2019-07-18 14:30:11 +0900

    https://atcoder.jp/contests/arc045/tasks/arc045_a

---
+++ AtCoder/arc045/arc045_a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string res(string str){
+    if(str == "Left"){
+        return "<";
+    }else if(str == "Right"){
+        return ">";
+    }else{
+        return "A";
+    }
+}
+
+int main(){
+    string str;
+    cin >> str;
+    cout << res(str);
+    while(cin >> str){
+        cout << " " << res(str); 
+    }
+    cout << "\n";
+}
