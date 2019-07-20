cbca09464efb2f59acfb6fd404fca85fe0492027
stone725 <inf33554432@gmail.com>
2019-07-20 09:42:32 +0900

    bitflyer2018_final_a

---
+++ AtCoder/bitflyer2018_final/bitflyer2018_final_a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int ans = 1e9 +7;
+    for(int i = 0; i < n; i++){
+        int num;
+        cin >> num;
+        int cnt = 0;
+        while(num % 10 == 0){
+            cnt++;
+            num /= 10;
+        }
+        ans = min(ans, cnt);
+    }
+    cout << ans << "\n";
+}
