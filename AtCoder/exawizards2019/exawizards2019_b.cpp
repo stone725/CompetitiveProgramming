36bf791724870c6fe1e4e557ba7aa31ae7acc2a8
stone725 <inf33554432@gmail.com>
2019-07-21 22:51:48 +0900

    exawizards2019_b

---
+++ AtCoder/exawizards2019/exawizards2019_b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+    if(count(begin(str), end(str), 'R') * 2 > n){
+        cout << "Yes\n";
+    }else{
+        cout << "No\n";   
+    }
+}
