92a424e908bbf889c0af6717b5fc7a8069d1e933
stone725 <inf33554432@gmail.com>
2019-07-24 15:40:29 +0900

    nikkei2019_qual_b

---
+++ AtCoder/nikkei2019_qual/nikkei2019_qual_b.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    string a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] != b[i]){
+            ans++;
+        }
+        if(b[i] != c[i]){
+            ans++;
+            if(a[i] == c[i]){
+                ans--;
+            }
+        }
+    }
+    cout << ans << "\n";
+}
