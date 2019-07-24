0f7bddce1ad37c2fde65e52db68585ce9203209b
stone725 <inf33554432@gmail.com>
2019-07-24 15:04:11 +0900

    nikkei2019ex_a

---
+++ AtCoder/nikkei2019ex/nikkei2019ex_a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    for(int i = 1; i <= str.size(); i++){
+        cout << i << "\n";
+    }
+}
