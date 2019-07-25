71bf4cba76b6d0fd417186146c5299e220cb5be6
stone725 <inf33554432@gmail.com>
2019-07-25 10:06:40 +0900

    chokudai_S002_c

---
+++ AtCoder/chokudai_S002/chokudai_S002_c.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        int a, b;
+        cin >> a >> b;
+        ans = max(ans, a + b);
+    }
+    cout << ans << "\n";
+}
