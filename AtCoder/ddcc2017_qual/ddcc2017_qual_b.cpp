3bda79e4d71480a146489a00f6fc9a1406d6cbfa
stone725 <inf33554432@gmail.com>
2019-07-26 12:56:51 +0900

    ddcc2017_qual_b

---
+++ AtCoder/ddcc2017_qual/ddcc2017_qual_b.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    long long ans = a * 1728 + b * 144 + c * 12 + d;
+    cout << ans << "\n";
+}
