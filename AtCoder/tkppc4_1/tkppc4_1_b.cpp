32217ecc7c0ddbbcdf1107cf31698e3139c373ef
stone725 <inf33554432@gmail.com>
2019-07-28 07:28:43 +0900

    tkppc4_1_b

---
+++ AtCoder/tkppc4_1/tkppc4_1_b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    int ans = -1, score = -1;
+    for(int i = 0; i < n; i++){
+        int num;
+        cin >> num;
+        if(num < k && score < num){
+            ans = i + 1;
+            score = num;
+        }
+    }
+    cout << ans << "\n";
+}
