a3d5334558bdc2be80f65d17f26ab91f5ee74410
stone725 <inf33554432@gmail.com>
2019-07-28 07:34:59 +0900

    tkppc4_1_c

---
+++ AtCoder/tkppc4_1/tkppc4_1_c.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    long long n;
+    string str;
+    cin >> n >> str;
+    for(int i = 2; i <= 10; i++){
+        long long score = 0;
+        for(int j = 0; j < str.size(); j++){
+            if(i <= str[j] - '0' || n < score){
+                score = -1;
+                break;
+            }
+            score *= i;
+            score += str[j] - '0';
+        }
+        if(score == n){
+            cout << i << "\n";
+        }
+    }
+}
