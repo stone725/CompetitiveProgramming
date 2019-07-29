0bd7a8167ba3d1b6cc6509f294baa4082817e914
stone725 <inf33554432@gmail.com>
2019-07-29 19:54:19 +0900

    yahoo_procon2017_qual_b

---
+++ AtCoder/yahoo_procon2017_qual/yahoo_procon2017_qual_b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    sort(begin(a), end(a));
+    cout << accumulate(begin(a), begin(a) + k, k * (k - 1ll) / 2) << "\n";
+}
