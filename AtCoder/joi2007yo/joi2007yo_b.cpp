1004f77ec59990f987a7451d748c8c997b59bd8d
stone725 <inf33554432@gmail.com>
2019-07-31 08:04:22 +0900

    joi2007yo_b

---
+++ AtCoder/joi2007yo/joi2007yo_b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    bool solved[31] = {};
+    for(int i = 0; i < 28; i++){
+        int num;
+        cin >> num;
+        solved[num] = true;
+    }
+    bool first = true;
+    for(int i = 1; i <= 30; i++){
+        if(solved[i]) continue;
+        cout << i << "\n";
+    }
+}
