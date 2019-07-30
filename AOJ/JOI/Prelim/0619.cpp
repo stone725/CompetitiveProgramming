8aa5f3c1bd8694da8c061fc4895a90648e2354c0
stone725 <inf33554432@gmail.com>
2019-07-30 21:03:40 +0900

    AOJ0619

---
+++ AOJ/JOI/Prelim/0619.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int main(){
+    int a[4], b[2];
+    for(auto&& i : a){
+        cin >> i;
+    }
+    for(auto&& j : b){
+        cin >> j;
+    }
+    sort(rbegin(a), rend(a));
+    sort(rbegin(b), rend(b));
+    cout << accumulate(begin(a), begin(a) + 3, 0) + b[0] << "\n"; 
+}
