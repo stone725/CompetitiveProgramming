ade71a7d5b1aab6fe52a949e47be00005a16cfb5
stone725 <inf33554432@gmail.com>
2019-07-30 21:09:45 +0900

    AOJ0620

---
+++ AOJ/JOI/Prelim/0620.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    for(int k = 1; k <= m; k++){
+        for(int j = 0; j < n - 1; j++){
+            if(a[j] % k > a[j + 1] % k){
+                swap(a[j], a[j + 1]);
+            }
+        }
+    }
+    for(auto&& i : a){
+        cout << i << "\n";
+    }
+}
