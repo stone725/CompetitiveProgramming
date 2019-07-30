571b0d0fbacee53621b938d260da74a16b3a22aa
stone725 <inf33554432@gmail.com>
2019-07-30 18:41:08 +0900

    tenka1_2017_b

---
+++ AtCoder/tenka1_2017/tenka1_2017_b.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int score = 1e9 + 7, num = 0;
+    for(int i = 0; i < n; i++){
+        int a, b;
+        cin >> a >> b;
+        if(score > b){
+            num = a;
+            score = b;
+        }
+    }
+    cout << num + score << "\n";
+}
