5ac7154c2cee3116f606b2ad85d7e03be04ff3fb
stone725 <inf33554432@gmail.com>
2019-07-30 18:52:27 +0900

    caddi2018b_b

---
+++ AtCoder/caddi2018b/caddi2018b_b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, h, w;
+    cin >> n >> h >> w;
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        int a, b;
+        cin >> a >> b;
+        ans += h <= a && w <= b;
+    }
+    cout << ans << "\n";
+}
