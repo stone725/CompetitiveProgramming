017da647df32fb57d7d48a8b7a415528d2f25aca
stone725 <inf33554432@gmail.com>
2019-07-30 19:07:18 +0900

    keyence2019_b

---
+++ AtCoder/keyence2019/keyence2019_b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    for(int j = 0; j <= 7; j++){
+        if(s.substr(0, j) + s.substr(s.size() - 7 + j) == "keyence"){
+            cout << "YES\n";
+            return 0;
+        }
+    }
+    cout << "NO\n";
+}
