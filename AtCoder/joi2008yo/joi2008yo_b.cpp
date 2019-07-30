85a73a11e291f774c11fa6919afdb83ed84b4186
stone725 <inf33554432@gmail.com>
2019-07-31 08:13:03 +0900

    joi2008yo_b

---
+++ AtCoder/joi2008yo/joi2008yo_b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    int ans[2] = {};
+    cin >> str;
+    for(int i = 0; i + 3 <= str.size(); i++){
+        ans[0] += str.substr(i, 3) == "JOI";
+        ans[1] += str.substr(i, 3) == "IOI";
+    }
+    for(auto&& i : ans){
+        cout << i << "\n";
+    }
+}
