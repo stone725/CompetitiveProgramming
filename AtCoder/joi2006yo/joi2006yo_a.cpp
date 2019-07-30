4cc083df17e2c299f51a35f8e7ccd6f15c4afd9d
stone725 <inf33554432@gmail.com>
2019-07-31 07:53:18 +0900

    joi2006yo_a

---
+++ AtCoder/joi2006yo/joi2006yo_a.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int ascore = 0, bscore = 0;
+    for(int i = 0; i < n; i++){
+        int a, b;
+        cin >> a >> b;
+        if(a < b){
+            bscore += a + b;
+        }else if(b < a){
+            ascore += a + b;
+        }else{
+            ascore += a;
+            bscore += b;
+        }
+    }
+    cout << ascore << " " << bscore << "\n";
+}
