532a36fc02efdac6f8ff79167ffc2a912057d6f9
stone725 <inf33554432@gmail.com>
2019-07-31 07:57:13 +0900

    joi2006yo_b

---
+++ AtCoder/joi2006yo/joi2006yo_b.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    map<char, char> change;
+    for(int i = 0; i < n; i++){
+        char from, to;
+        cin >> from >> to;
+        change[from] = to;
+    }
+    int m;
+    cin >> m;
+    for(int i = 0; i < m; i++){
+        char c;
+        cin >> c;
+        if(change.count(c)){
+            cout << change[c];
+        }else{
+            cout << c;
+        }
+    }
+    cout << "\n";
+}
