13a55f811d601a90fd3608d5aa3a438f03a0047e
stone725 <inf33554432@gmail.com>
2019-07-31 19:30:09 +0900

    https://atcoder.jp/contests/agc023/tasks/agc023_a

---
+++ AtCoder/agc023/agc023_a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <numeric>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<long long> a(n), b(n + 1);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    partial_sum(begin(a), end(a), begin(b) + 1);
+    long long ans = 0;
+    map<long long, int> cnt;
+    for(int i = 1; i <= n; i++){
+        if(b[i] == 0) ans++;
+        ans += cnt[b[i]];
+        cnt[b[i]]++;
+    }
+    cout << ans << "\n";
+}
