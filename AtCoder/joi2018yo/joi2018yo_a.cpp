0ade314aece606a50a8032ccf73cf66bbb5615d5
stone725 <inf33554432@gmail.com>
2019-07-31 21:24:44 +0900

    joi2018_yo_a

---
+++ AtCoder/joi2018yo/joi2018yo_a.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, a, b, c, d;
+    cin >> n >> a >> b >> c >> d;
+    cout << min(b * (n / a + !!(n % a)), d * (n / c + !!(n % c))) << "\n";
+}
