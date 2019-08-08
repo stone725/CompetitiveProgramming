1b452afc661eb717b55ac979cf82ac71c8b5b6a4
stone725 <inf33554432@gmail.com>
2019-08-09 04:44:48 +0900

    https://yukicoder.me/problems/no/799

---
+++ yukicoder/799.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if(b < c || d < a){
+        cout << (b - a + 1) * (d - c + 1) << "\n";
+        return 0;
+    }
+    cout << (b - a + 1) * (d - c + 1) - (min(d, b) - max(a, c) + 1) << "\n";
+}
