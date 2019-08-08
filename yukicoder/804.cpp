08dffbc380f6bee4c53af525b1dd30c221feb67f
stone725 <inf33554432@gmail.com>
2019-08-09 04:36:35 +0900

    https://yukicoder.me/problems/no/804

---
+++ yukicoder/804.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    for(int i = a; i >= 0; i--){
+        if(i * c <= b && i * (c + 1) <= d){
+            cout << i << "\n";
+            return 0;
+        }
+    }
+    return 0;
+}
