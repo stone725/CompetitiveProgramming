97e44e15977a3f9bb686935bc8af8c80ccf86e3b
stone725 <inf33554432@gmail.com>
2019-08-08 16:25:12 +0900

    https://atcoder.jp/contests/abc105/tasks/abc105_c

---
+++ AtCoder/abc105/abc105_c.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+long long mod(long long n, int m){
+    return ((n % m) + m) % m;
+}
+
+int main(){
+    long long n;
+    cin >> n;
+    string ans = "";
+    long long checkint = 1;
+    do{
+        switch(mod(n / abs(checkint), 2)){
+        case 0:
+            ans = "0" + ans;
+            break;
+        case 1:
+            ans = "1" + ans;
+            n -= checkint;
+            break;
+        }
+        checkint *= -2;
+    }while(n);
+    cout << ans << "\n";
+}
