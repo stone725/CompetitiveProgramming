e96151f2883698e0e8a2d7d958988584b2e60f50
stone725 <inf33554432@gmail.com>
2019-08-10 06:28:43 +0900

    caddi2018_a

---
+++ AtCoder/caddi2018/caddi2018_a.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    long long n, p;
+    cin >> n >> p;
+    long long ans = 1;
+    if(n == 1){
+        cout << p << "\n";
+        return 0;
+    }
+    for(int i = sqrt(p); i > 1; i--){
+        if(p % i) continue;
+        long long score = p;
+        bool check = true;
+        for(int j = 0; j < n; j++){
+            if(score % i){
+                check = false;
+                break;
+            }
+            score /= i;
+        }
+        if(check){
+            ans = i;
+            break;
+        }
+    }
+    cout << ans << "\n";
+}
