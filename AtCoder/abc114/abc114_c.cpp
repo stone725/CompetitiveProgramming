d84ce45af677b53d044f7a7de86e244637cb41ee
stone725 <inf33554432@gmail.com>
2019-08-09 10:24:31 +0900

    https://atcoder.jp/contests/abc114/tasks/abc114_c

---
+++ AtCoder/abc114/abc114_c.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int n;
+
+int dfs(unsigned int score, bitset<3> bits){
+    if(score > n) return 0;
+    int res = bits.count() == 3;
+    auto nextbits = bits;
+    for(int i = 3; i <= 7; i += 2){
+        nextbits[i - 3 / 2] = true;
+        res += dfs(score * 10 + i, nextbits);
+        nextbits[i - 3 / 2] = bits[i - 3 / 2];
+    }
+    return res;
+}
+
+int main(){
+    cin >> n;
+    cout << dfs(0, bitset<3>(0)) << "\n";
+}
