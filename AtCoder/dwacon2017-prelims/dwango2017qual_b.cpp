944ee011dcbb5a89e4cfdd4fbfd13983ebf049b5
stone725 <inf33554432@gmail.com>
2019-08-11 05:03:28 +0900

    dwango2017qual_b

---
+++ AtCoder/dwacon2017-prelims/dwango2017qual_b.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+string str;
+int dp[100000][2], score;
+
+int dfs(int now, bool five){
+    if(now == str.size()){
+        return 0;
+    }
+    if(~dp[now][five]) return dp[now][five];
+    int res = 0;
+    if(str[now] == '?'){
+        res = 1 + dfs(now + 1, !five);
+    }else if(str[now] == '2' && !five){
+        res = 1 + dfs(now + 1, !five);
+    }else if(str[now] == '5' && five){
+        res = 1 + dfs(now + 1, !five);
+    }
+    score = max(score, dfs(now + 1, false));
+    return dp[now][five] = res;
+}
+
+int main(){
+    memset(dp, -1, sizeof dp);
+    cin >> str;
+    int ans = max(dfs(0, 0), score);
+    cout << ans - (ans % 2) << "\n";
+}
