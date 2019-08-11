fc05d5433415148c754c3e4dfbec5933dac93003
stone725 <inf33554432@gmail.com>
2019-08-11 17:36:41 +0900

    colopl2018_final_a

---
+++ AtCoder/colopl2018-final-open/colopl2018_final_a.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using  namespace std;
+
+int main(){
+    long long n;
+    cin >> n;
+    string str;
+    cin >> str;
+    long long ans = 0;
+    long long cnt = 0;
+    if(str == string(str.size(), 'A')){
+        cout << (n * str.size()) * (n * str.size() + 1) / 2;
+        return 0;
+    }
+    bool first = true;
+    long long start = 0;
+    for(int i = 0; i < str.size(); i++){
+        if(str[i] == 'A'){
+            cnt++;
+        }else{
+            if(first){
+                start = cnt;
+                first = false;
+            }else{
+                ans += cnt * (cnt + 1) / 2;
+            }
+            cnt = 0;
+        }
+    }
+    ans *= n;
+    ans += start * (start + 1) / 2;
+    ans += cnt * (cnt + 1) / 2;
+    ans += (n - 1) * (start + cnt) * (start + cnt + 1) / 2;
+    cout << ans << "\n";
+}
