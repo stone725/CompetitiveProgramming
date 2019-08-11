924a6a061fe9e0d4ec4938e43af21f1caaa719d1
stone725 <inf33554432@gmail.com>
2019-08-12 08:56:10 +0900

    ECR1A

---
+++ CodeForces/ECR1/A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long num;
+        cin >> num;
+        long long ans = num * (num + 1) / 2;
+        for(long long i = 1; i <= num; i *= 2){
+            ans -= 2 * i;
+        }
+        cout << ans << "\n";
+    }
+}
