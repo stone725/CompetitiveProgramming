3f0c9fca547ee6ffaef688a5c5d04bc190a8d910
stone725 <inf33554432@gmail.com>
2019-08-12 19:05:06 +0900

    CR244B

---
+++ CodeForces/CR244/B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, t, c;
+    cin >> n >> t >> c;
+    int cnt = 0, ans = 0;
+    for(int i = 0; i < n; i++){
+        int a;
+        cin >> a;
+        if(t < a){
+            ans += max(0, cnt - (c - 1));
+            cnt = 0;
+        }else{
+            cnt++;
+        }
+    }
+    ans += max(0, cnt - (c - 1));
+    cout << ans << "\n";
+}
