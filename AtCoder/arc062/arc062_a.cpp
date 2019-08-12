927f012b4484127bb54d6c509b0f2fd3ac6de864
stone725 <inf33554432@gmail.com>
2019-08-12 14:21:19 +0900

    https://atcoder.jp/contests/arc062/tasks/arc062_a

---
+++ AtCoder/arc062/arc062_a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    long long scoreA = 0, scoreB = 0;
+    while(n--){
+        int a, b;
+        cin >> a >> b;
+        if(scoreA == scoreB && !scoreA){
+            scoreA = a;
+            scoreB = b;
+            continue;
+        }
+        long long time = max(scoreA / a + !!(scoreA % a), scoreB / b + !!(scoreB % b));
+        scoreA = a * time;
+        scoreB = b * time;
+    }
+    cout << scoreA + scoreB << "\n";
+}
