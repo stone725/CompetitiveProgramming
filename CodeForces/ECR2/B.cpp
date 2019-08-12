6d0a1a347711c17217ffcefebb6eb21367cb250b
stone725 <inf33554432@gmail.com>
2019-08-12 13:14:41 +0900

    ECR2B

---
+++ CodeForces/ECR2/B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    for(auto&& i : b){
+        cin >> i;
+    }
+    sort(begin(a), end(a));
+    for(int i = 0; i < m - 1; i++){
+        cout << static_cast<int>(upper_bound(begin(a), end(a), b[i]) - begin(a)) << " ";
+    }
+    cout << static_cast<int>(upper_bound(begin(a), end(a), b[m - 1]) - begin(a)) <<"\n";
+}
