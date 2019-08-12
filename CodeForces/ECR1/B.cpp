04e0221677ffc514ae7b8fbefabb75f4bdacb088
stone725 <inf33554432@gmail.com>
2019-08-12 09:16:22 +0900

    ECR1B

---
+++ CodeForces/ECR1/B.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    int t;
+    cin >> t;
+    while(t--){
+        cerr << str << "\n";
+        int l, r, k;
+        cin >> l >> r >> k;
+        if(l == r) continue;
+        l--;
+        if(k > r - l){
+            k %= (r - l);
+        }
+        str = str.substr(0, l) + str.substr(r - k, k) + str.substr(l, r - l - k) + str.substr(r);
+    }
+    cout << str << "\n";
+}
