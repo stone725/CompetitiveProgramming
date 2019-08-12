cf3e6ee0d795f9ef4cbc5cd4299fcd69894dc971
stone725 <inf33554432@gmail.com>
2019-08-12 14:08:43 +0900

    ECR2C

---
+++ CodeForces/ECR2/C.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    int cnt[26] = {};
+    for(auto&& i : str){
+        cnt[i - 'a']++;
+    }
+    string center = "";
+    string ans = "";
+    for(int i = 0; i < 26; i++){
+        if(cnt[i] % 2){
+            for(int j = 25; j >= i; j--){
+                if(i == j){
+                    center = string(1, 'a' + i);
+                    break;
+                }else if(cnt[j] % 2){
+                    cnt[j]--;
+                    cnt[i]++;
+                    break;
+                }
+            }
+        }
+        ans += string(cnt[i] / 2, 'a' + i);
+    }
+    cout << ans + center + string(rbegin(ans), rend(ans)) << "\n";
+}
