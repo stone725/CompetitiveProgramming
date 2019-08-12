abe037fc8940efc80d9b16bdd47714f5058579cf
stone725 <inf33554432@gmail.com>
2019-08-12 17:40:49 +0900

    CoderStrike2014R1A

---
+++ CodeForces/CoderStrike2014R1/A.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    string str;
+    cin >> str;
+    k--;
+    int now = k;
+    vector<bool> printed(n, false);
+    while(true){
+        if(!printed[now]){
+            cout << "PRINT " << str[now] << "\n";
+            printed[now] = true;
+            if(printed == vector<bool>(n, true)){
+                return 0;
+            }
+        }
+        if(now == n - 1){
+            cout << "LEFT\n";
+            now--;
+        }else if(now == 0){
+            cout << "RIGHT\n";
+            now++;
+        }else if(n - 1 - now > now && !printed[0]){
+            cout << "LEFT\n";
+            now--;
+        }else if(n - 1 - now < now && !printed[n - 1]){
+            cout << "RIGHT\n";
+            now++;
+        }else if(printed[0]){
+            cout << "RIGHT\n";
+            now++;
+        }else{
+            cout << "LEFT\n";
+            now--;
+        }
+    }
+}
