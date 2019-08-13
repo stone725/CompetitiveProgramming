282831486d31bb16765b028daed8738ab3bd6c19
stone725 <inf33554432@gmail.com>
2019-08-13 14:13:46 +0900

    ECR5A

---
+++ CodeForces/ECR5/A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string a, b;
+    cin >> a >> b;
+    int num;
+    for(num = 0; num + 1 < a.size() && a[num] == '0'; num++);
+    a = a.substr(num);
+    for(num = 0; num + 1 < b.size() && b[num] == '0'; num++);
+    b = b.substr(num);  
+    if(a.size() < b.size()){
+        cout << "<\n";
+        return 0;
+    }else if(a.size() > b.size()){
+        cout << ">\n";
+        return 0;
+    }
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] < b[i]){
+            cout << "<\n";
+            return 0;
+        }else if(a[i] > b[i]){
+            cout << ">\n";
+            return 0;
+        }
+    }
+    cout << "=" << "\n";
+}
