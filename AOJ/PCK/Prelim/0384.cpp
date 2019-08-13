08b6a1566a4cba77b7e4cec14e1fdbd19ce3f14e
stone725 <inf33554432@gmail.com>
2019-08-13 17:29:54 +0900

    AOJ0384

---
+++ AOJ/PCK/Prelim/0384.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int a, n, m;
+    cin >> a >> n >> m;
+    int ans = 0;
+    for(long long i = a; pow(i, n) <= m; i++){
+        long long score = 1;
+        for(int j = 0; j < n; j++){
+            score *= i;
+        }
+        if(score == 0) continue;
+        long long num = i - a;
+        while(score){
+            num -= (score % 10);
+            score /= 10;
+        }
+        ans += !num;
+    }
+    cout << ans << "\n";
+}
