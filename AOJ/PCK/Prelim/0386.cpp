7f2ccb1d79c5503c2249f99fa901bcfc5ec6b7f5
stone725 <inf33554432@gmail.com>
2019-08-13 19:34:04 +0900

    AOJ0386

---
+++ AOJ/PCK/Prelim/0386.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    static const int INF = 1e9 + 7;
+    int n;
+    string s, t;
+    long long dp[26] = {};
+    cin >> n;
+    cin >> s;
+    cin >> t;
+    dp[s[0] - 'a'] = 1;
+    for(int i = 1; i < n - 1; i++){
+        dp[s[i] - 'a'] += dp[t[i] - 'a'];
+        dp[s[i] - 'a'] %= INF;
+    }
+    cout << dp[t[n - 1] - 'a'] << "\n";
+}
