1948e3d71b4ea4416b4d00326f08d7dced7709a9
stone725 <inf33554432@gmail.com>
2019-08-13 17:03:35 +0900

    AOJ0382

---
+++ AOJ/PCK/Prelim/0382.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int main(){
+    int n, c;
+    cin >> n >> c;
+    vector<int> cakes(c);
+    for(auto&& i : cakes){
+        cin >> i;
+    }
+    int sum = accumulate(begin(cakes), end(cakes), 0);
+    cout << sum / (n + 1) + !!(sum % (n + 1)) << "\n";
+}
