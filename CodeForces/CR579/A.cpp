9c1892049001eed34f853fd879f2f3bf6b06777d
stone725 <inf33554432@gmail.com>
2019-08-14 19:04:54 +0900

    CR579A

---
+++ CodeForces/CR579/A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int q;
+    cin >> q;
+    while(q--){
+        int n;
+        cin >> n;
+        list<int> p(n);
+        for(auto&& i : p){
+            cin >> i;
+        }
+        bool check = false;
+        for(int i = 0; i < n; i++){
+            check = is_sorted(begin(p), end(p)) || is_sorted(rbegin(p), rend(p));
+            if(check){
+                break;
+            }
+            p.push_back(p.front());
+            p.pop_front();
+        }
+        if(check){
+            cout << "YES\n";
+        }else{
+            cout << "NO\n";
+        }
+    }
+}
