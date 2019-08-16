4fb6fda66b5075d649095a5c58b3b775b03306e4
stone725 <inf33554432@gmail.com>
2019-08-16 16:43:13 +0900

    joi2010yo_d

---
+++ AtCoder/joi2010yo/joi2010yo_d.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    vector<string> cards(n);
+    for(auto&& i : cards){
+        cin >> i;
+    }
+    sort(begin(cards), end(cards));
+    set<string> check;
+    do{
+        string num;
+        for(int i = 0; i < k; i++){
+            num += cards[i];
+        }
+        check.insert(num);
+    }while(next_permutation(begin(cards), end(cards)));
+    cout << check.size() << "\n";
+}
