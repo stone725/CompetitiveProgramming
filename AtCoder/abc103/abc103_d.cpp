e73221b4963553fa370e8dd1c5b18395023a9103
stone725 <inf33554432@gmail.com>
2019-08-16 19:37:13 +0900

    https://atcoder.jp/contests/abc103/tasks/abc103_d

---
+++ AtCoder/abc103/abc103_d.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+bool comp(pair<int, int> a, pair<int, int> b){
+    return a.second < b.second;
+}
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<pair<int, int>> a(m);
+    for(auto&& i : a){
+        cin >> i.first >> i.second;
+    }
+    sort(begin(a), end(a), comp);
+    int ans = 0, p = 0;
+    for(auto&& i : a){
+        if(p < i.first){
+            p = i.second - 1;
+            ans++;
+        }
+    }
+    cout << ans << "\n";
+}
