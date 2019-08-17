6288b704e46613d2e37c65cf84cc93fde8bc4a2b
stone725 <inf33554432@gmail.com>
2019-08-18 06:16:46 +0900

    https://atcoder.jp/contests/abc005/tasks/abc005_3

---
+++ AtCoder/abc005/abc005_3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    int m;
+    cin >> m;
+    vector<int> b(m);
+    for(auto&& i : b){
+        cin >> i;
+    }
+    if(n < m){
+        cout << "no\n";
+        return 0;
+    }
+    auto p = begin(a);
+    for(int i = 0; i < m; i++){
+        p = lower_bound(p, end(a), b[i] - t);
+        if(p == end(a) || b[i] < *p){
+            cout << "no\n";
+            return 0;
+        }
+        p++;
+    }
+    cout << "yes\n";
+}
