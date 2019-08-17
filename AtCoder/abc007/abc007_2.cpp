97315ea5771aa462aa85c97d3eb119f2a18a9f99
stone725 <inf33554432@gmail.com>
2019-08-17 15:37:19 +0900

    https://atcoder.jp/contests/abc007/tasks/abc007_2

---
+++ AtCoder/abc007/abc007_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    if(str == "a"){
+        cout << "-1\n";
+    }else{
+        cout << "a\n";
+    }
+}
