efd5f34c5ea56d581b91717b2d70ae2427892d0c
stone725 <inf33554432@gmail.com>
2019-08-17 15:34:50 +0900

    https://atcoder.jp/contests/abc076/tasks/abc076_c

---
+++ AtCoder/abc076/abc076_c.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string s, t;
+    cin >> s >> t;
+    if(s.size() < t.size()){
+        cout << "UNRESTORABLE\n";
+        return 0;
+    }
+    bool check;
+    for(int i = s.size() - t.size(); i >= 0; i--){
+        check = true;
+        for(int j = 0; j < t.size(); j++){
+            if(s[i + j] != t[j] && s[i + j] != '?'){
+                check = false;
+                break;
+            }
+        }
+        if(check){
+            s = s.substr(0, i) + t + s.substr(i + t.size());
+            break;
+        }
+    }
+    if(check){
+        for(auto&& i : s){
+            if(i == '?'){
+                cout << "a";
+            }else{
+                cout << i;
+            }
+        }
+        cout << "\n";
+    }else{
+        cout << "UNRESTORABLE\n";
+    }
+}
