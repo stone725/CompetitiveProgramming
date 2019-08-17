c0c4d6ffa1ac16e994d8e32d9306c3e5a5330c1d
stone725 <inf33554432@gmail.com>
2019-08-17 16:36:29 +0900

    https://atcoder.jp/contests/abc083/tasks/arc088_a

---
+++ AtCoder/arc088/arc088_a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long long x, y;
+    cin >> x >> y;
+    long long ans = 0;
+    while(x <= y){
+        ans++;
+        x *= 2;
+    }
+    cout << ans << "\n";
+}
