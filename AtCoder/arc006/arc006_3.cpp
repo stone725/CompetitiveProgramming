b81dc396fe7ab66fd07c755aee2c5b83f3a0704a
stone725 <inf33554432@gmail.com>
2019-08-18 06:09:27 +0900

    https://atcoder.jp/contests/arc006/tasks/arc006_3

---
+++ AtCoder/arc006/arc006_3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> groups;
+    for(int i = 0; i < n; i++){
+        int w;
+        cin >> w;
+        if(lower_bound(begin(groups), end(groups), w) != end(groups)){
+            *lower_bound(begin(groups), end(groups), w) = w;
+        }else{
+            groups.push_back(w);
+        }
+        sort(begin(groups), end(groups));
+    }
+    cout << groups.size() << "\n";
+}
