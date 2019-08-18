4f5e2ee80da4468b0c506c1a95dd7dd92e47f93a
stone725 <inf33554432@gmail.com>
2019-08-18 18:51:15 +0900

    https://atcoder.jp/contests/arc074/tasks/arc074_a

---
+++ AtCoder/arc074/arc074_a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    long long h, w;
+    cin >> h >> w;
+    if(h % 3 == 0 || w % 3 == 0){
+        cout << 0 << "\n";
+        return 0;
+    }
+    long long score = min(w, h);
+    for(int i = 1; i < h; i++){
+        long long a = i * w;
+        long long b = (h - i) * (w / 2);
+        long long c = (h - i) * (w / 2 + w % 2);
+        score = min(score, max({a, b, c}) - min({a, b, c}));
+    }
+    for(int i = 1; i < w; i++){
+        long long a = i * h;
+        long long b = (w - i) * (h / 2);
+        long long c = (w - i) * (h / 2 + h % 2);
+        score = min(score, max({a, b, c}) - min({a, b, c}));
+    }
+    cout << score << "\n";
+}
