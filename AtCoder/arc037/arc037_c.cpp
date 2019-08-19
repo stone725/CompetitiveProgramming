f4a56fc5630652254752d3ef7d5c846477b18c22
stone725 <inf33554432@gmail.com>
2019-08-19 15:17:54 +0900

    https://atcoder.jp/contests/arc037/tasks/arc037_c

---
+++ AtCoder/arc037/arc037_c.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n;
+long long k;
+vector<long long> a, b;
+
+long long cnt(long long mid){
+    long long res = 0;
+    for(int i = 0; i < n; i++){
+        res += upper_bound(begin(b), end(b), mid / a[i]) - begin(b);
+    }
+    return res;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n >> k;
+    a.resize(n);
+    b.resize(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    for(auto&& i : b){
+        cin >> i;
+    }
+    sort(begin(a), end(a));
+    sort(begin(b), end(b));
+    long long s = 0, g = a.back() * b.back();
+    while(g - s > 1){
+        long long mid = (s + g) / 2;
+        if(k <= cnt(mid)){
+            g = mid;
+        }else{
+            s = mid;
+        }
+    }
+    cout << g << "\n";
+}
