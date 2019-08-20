20a8d9947426e00c0f9e160b0e72579a1b2d546d
stone725 <inf33554432@gmail.com>
2019-08-20 15:15:38 +0900

    https://atcoder.jp/contests/arc022/tasks/arc022_2

---
+++ AtCoder/arc022/arc022_2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    set<int> s;
+    int l = 0, r = 0;
+    int ans = 0;
+    while(r < n){
+        if(l == r){
+            s.insert(a[r]);
+            r++;
+            ans = max<int>(ans, s.size());
+            continue;
+        }
+        while(s.count(a[r])){
+            s.erase(a[l]);
+            l++;
+        }
+        s.insert(a[r]);
+        r++;
+        ans = max<int>(ans, s.size());
+    }
+    cout << ans << "\n";
+}
