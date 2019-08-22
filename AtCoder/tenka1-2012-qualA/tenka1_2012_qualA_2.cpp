33c830cba994434db56abc85b119af27644943cd
stone725 <inf33554432@gmail.com>
2019-08-22 09:33:13 +0900

    tenka1_2012_qualA_2

---
+++ AtCoder/tenka1-2012-qualA/tenka1_2012_qualA_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    cout << str;
+    while(cin >> str){
+        cout << "," << str;
+    }
+    cout << "\n";
+}
