18629b1e9a2240134ae514b1a581e07be9b586a3
stone725 <inf33554432@gmail.com>
2019-08-23 20:43:36 +0900

    https://atcoder.jp/contests/arc091/tasks/arc091_a

---
+++ AtCoder/arc091/arc091_a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    long long n, m;
+    cin >> n >> m;
+    if(m < n) swap(n, m);
+    if(n == 1){
+        if(m == 1){
+            cout << 1 << "\n";
+        }else{
+            cout << m - 2 << "\n";
+        }
+        return 0;
+    }
+    cout << (n - 2) * (m - 2) << "\n";
+}
