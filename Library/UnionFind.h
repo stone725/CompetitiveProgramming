aa0ac45cd7a5d1780a87693f9c895a332ae2a99d
stone725 <inf33554432@gmail.com>
2019-08-26 00:46:51 +0900

    ライブラリにUnionFindを追加

---
+++ Library/UnionFind.h
@@ -0,0 +1,51 @@
+#include <vector>
+
+struct Union_Find {
+    std::vector<int> par;
+    std::vector<int> rank;
+    
+    //初期化
+    void init( int n ) {
+        par.resize( n );
+        rank.resize( n );
+        for ( int i = 0; i < n; ++i ) {
+            par[ i ] = i;
+            rank[ i ] = 0;
+        }
+    }
+    
+    //木の根を探す
+    int find( int s ) {
+        if ( par[ s ] == s ) {
+            return s;
+        }
+        return par[ s ] = find( par[ s ] );
+    }
+    
+    //結合
+    void unite( int x, int y ) {
+        int a = find( x ), b = find( y );
+        if ( a == b ) {
+            return;
+        }
+        if ( rank[ a ] < rank[ b ] ) {
+            par[ a ] = b;
+        }
+        else {
+            par[ a ] = b;
+            if ( rank[ a ] == rank[ b ] ) {
+                ++rank[ b ];
+            }
+        }
+    }
+    
+    //二つの数の根が同じかどうか
+    bool same( int x, int y ) {
+        return find( x ) == find( y );
+    }
+    
+    //サイズを返す
+    unsigned int size() {
+        return par.size();
+    }
+};
