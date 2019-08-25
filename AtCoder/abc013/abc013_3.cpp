6d1b0a2da62ac2e2a34b4bda87eba13b2b058c0f
stone725 <inf33554432@gmail.com>
2019-08-25 21:25:04 +0900

    https://atcoder.jp/contests/abc013/tasks/abc013_3

---
+++ AtCoder/abc013/abc013_3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+static const long long INF = 1ll << 60;
+long long n, h, a, b, c, d, e;
+
+long long calc(int day){
+    long long score = e * (n - day) - b * day - h;
+    if(0 <= score) return day * a + c * (((n - day) * e - h - b * day) / (d + e) + 1);
+    return day * a;
+}
+
+int main(){
+    cin >> n >> h;
+    cin >> a >> b >> c >> d >> e;
+    long long ans = INF;
+    for(int i = 0; i <= n; i++){
+        ans = min(ans, calc(i));
+    }
+    cout << ans << "\n";
+}
