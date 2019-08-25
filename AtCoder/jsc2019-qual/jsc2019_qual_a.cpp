24fd4d7df7effb8e44fa5ddbba96141341bc62a1
stone725 <inf33554432@gmail.com>
2019-08-25 12:35:03 +0900

    https://atcoder.jp/contests/jsc2019-qual

---
+++ AtCoder/jsc2019-qual/jsc2019_qual_a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int m, d;
+    cin >> m >> d;
+    int ans = 0;
+    for(int i = 1; i <= m; i++){
+        for(int j = 10; j <= d; j++){
+            ans += (j % 10 >= 2) && (j / 10 >= 2) && ((j / 10) * (j % 10) == i);
+        }
+    }
+    cout << ans << "\n";
+}
---
+++ AtCoder/jsc2019-qual/jsc2019_qual_b.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long modinv(long long a, long long m) {
+    long long b = m, u = 1, v = 0;
+    while (b) {
+        long long t = a / b;
+        a -= t * b; swap(a, b);
+        u -= t * v; swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
+
+
+int main(){
+    static const int INF = 1e9 + 7;
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    long long ans = 0;
+    for(int i = 0; i < n; i++){
+        long long left = 0, right = 0;
+        for(int j = 0; j < i; j++){
+            left += a[j] < a[i];
+        }
+        for(int j = i + 1; j < n; j++){
+            right += a[j] < a[i];
+        }
+        unsigned long long add = (left + right);
+        add *= (k - 1);
+        add %= INF;
+        add *= k;
+        add %= INF;
+        add *= modinv(2, INF);
+        add %= INF;
+        add += right * k;
+        ans += add;
+        ans %= INF;
+    }
+    cout << ans << "\n";
+}
