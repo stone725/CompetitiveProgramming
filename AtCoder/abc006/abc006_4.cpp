1876c89813cd4d97b72358d5b0141fedb58f91cd
stone725 <inf33554432@gmail.com>
2019-08-27 19:28:07 +0900

    https://atcoder.jp/contests/abc006/tasks/abc006_4

---
+++ AtCoder/abc006/abc006_4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> c(n), dp(n, -1);
+    for(auto&& i : c){
+        cin >> i;
+    }
+    int ans = n;
+    for(int i = 0; i < n; i++){
+        dp[i] = 1;
+        for(int j = 0; j < i; j++){
+            if(c[j] < c[i]){
+                dp[i] = max(dp[i], 1 + dp[j]);
+            }
+        }
+        ans = min(ans, n - dp[i]);
+    }
+    cout << ans << "\n";
+}
