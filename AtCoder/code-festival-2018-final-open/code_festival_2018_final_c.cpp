7eafcc62d7e14d9ee4d0a193a8892c3421ecc841
stone725 <inf33554432@gmail.com>
2019-08-30 10:34:00 +0900

    code_festival_2018_final_c

---
+++ AtCoder/code-festival-2018-final-open/code_festival_2018_final_c.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<pair<int, int>> plan(n);
+    for(auto&& i : plan){
+        cin >> i.first >> i.second;
+    }
+    sort(begin(plan), end(plan));
+    int m;
+    cin >> m;
+    while(m--){
+        int t;
+        cin >> t;
+        auto p = lower_bound(begin(plan), end(plan), make_pair(t, 0));
+        if(p == end(plan)){
+            cout << (p - 1)->second + (t - (p - 1)->first) << "\n";
+            continue;
+        }
+        if(p == begin(plan) || p->first == t){
+            cout << p->second << "\n";
+            continue;
+        }
+        cout << min(p->second, (p - 1)->second + t - (p - 1)->first) << "\n";
+    }
+}
