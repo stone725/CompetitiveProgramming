7562d9be8a9275300648d44748ea5055a0a62347
stone725 <inf33554432@gmail.com>
2019-08-30 11:28:23 +0900

    code_thanks_festival_2018_d

---
+++ AtCoder/code-thanks-festival-2018-open/code_thanks_festival_2018_d.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string str;
+
+int cnt(int p){
+    for(int i = p + 1; i < str.size(); i++){
+        if(str[i] <= str[p]){
+            return 1 + cnt(i);
+        }
+    }
+    return 1;
+}
+
+int main(){
+    cin >> str;
+    cout << cnt(0) << "\n";
+}
