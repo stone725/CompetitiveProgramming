1edefb3b3c4c15f4f48cdc4977eab505a9d56d2d
stone725 <inf33554432@gmail.com>
2019-08-31 18:44:43 +0900

    https://atcoder.jp/contests/agc002/tasks/agc002_c

---
+++ AtCoder/agc002/agc002_c.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n, l;
+    cin >> n >> l;
+    vector<int> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    for(int i = 1; i < n; i++){
+        if(l <= a[i] + a[i - 1]){
+            cout << "Possible\n";
+            for(int j = 0; j < i - 1; j++){
+                cout << j + 1 << "\n";
+            }
+            for(int j = n - 1; j >= i; j--){
+                cout << j << "\n";
+            }
+            return 0;
+        }
+    }
+    cout << "Impossible\n";
+}
