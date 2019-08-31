a5c369e5b815de01d01a5dc61a920b8074244757
stone725 <inf33554432@gmail.com>
2019-08-31 13:40:21 +0900

    https://atcoder.jp/contests/agc003/tasks/agc003_b

---
+++ AtCoder/agc003/agc003_b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    long long ans = 0;
+    for(int i = 0; i < n; i++){
+        ans += a[i] / 2;
+        a[i] %= 2;
+        if(i + 1 < n){
+            ans += a[i] && a[i + 1];
+            a[i + 1] -= a[i] && a[i + 1];
+        }
+    }
+    cout << ans << "\n";
+}
