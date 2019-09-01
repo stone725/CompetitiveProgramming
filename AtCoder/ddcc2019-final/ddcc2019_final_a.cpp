f67f2f6ce191f761eeb07a9406e3cd9cb5bb5d01
stone725 <inf33554432@gmail.com>
2019-09-01 17:26:25 +0900

    ddcc2019_final_a

---
+++ AtCoder/ddcc2019-final/ddcc2019_final_a.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+    int cnt = 0, maxcnt = 0;
+    double ans = 0;
+    for(int i = 0; i < n; i++){
+        if(str[i] == '>'){
+            cnt++;
+        }else{
+            cnt = 0;
+        }
+        ans += 1. / (cnt + 1);
+        maxcnt = max(cnt, maxcnt);
+    }
+    if(maxcnt < n){
+        ans -= 1;
+        ans += 1. / (maxcnt + 2);
+    }
+    cout << setprecision(15) << ans << "\n";
+}
