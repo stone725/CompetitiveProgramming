2beff7403c6704265d6a00ba9fc2897a04c8d994
stone725 <inf33554432@gmail.com>
2019-09-14 18:56:20 +0900

    wupc2012_1

---
+++ AtCoder/wupc2012/wupc2012_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int month[] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int ma, da, mb, db;
+    cin >> ma >> da;
+    cin >> mb >> db;
+    int ans = 0;
+    for(int i = ma - 1; i < mb - 1; i++){
+        ans += month[i];
+    }
+    ans -= da;
+    ans += db;
+    cout << ans << "\n";
+}
