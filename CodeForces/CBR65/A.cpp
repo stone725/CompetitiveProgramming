6c2e0981935732f2a52061e285e1e6116147f845
stone725 <inf33554432@gmail.com>
2019-09-16 07:03:00 +0900

    https://codeforces.com/problemset/problem/71/A

---
+++ CodeForces/CBR65/A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        string str;
+        cin >> str;
+        if(str.size() <= 10){
+            cout << str << "\n";
+        }else{
+            cout << str[0] << str.size() - 2 << str[str.size() - 1] << "\n";
+        }
+    }
+}
