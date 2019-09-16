87001741be52168c39abce6b61a90a57298c0a3b
stone725 <inf33554432@gmail.com>
2019-09-16 09:39:35 +0900

    https://codeforces.com/problemset/problem/158/A

---
+++ CodeForces/VKC2012QR1/A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <utility>
+#include <map>
+
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    map<int, int> m;
+    for(int i = 0; i < n; i++){
+        int a;
+        cin >> a;
+        m[a]++;
+    }
+    int ans = 0;
+    for(auto i = m.rbegin(); i != m.rend() && i->first > 0; i++){
+        ans += i->second;
+        if(k <= ans){
+            break;
+        }
+    }
+    cout << ans << "\n";
+}
