c3b3ba8807b11e12818f5cc616a11fc330d65334
stone725 <inf33554432@gmail.com>
2019-09-16 10:11:59 +0900

    https://atcoder.jp/contests/arc049/tasks/arc049_a

---
+++ AtCoder/arc049/arc049_a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    str += '\n';
+    vector<bool> check(str.size(), false);
+    for(int i = 0; i < 4; i++){
+        int num;
+        cin >> num;
+        check[num] = true;
+    }
+    for(int i = 0; i < str.size(); i++){
+        if(check[i]){
+            cout << "\"";
+        }
+        cout << str[i];
+    }
+}
