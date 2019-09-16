6cf16c680bb733b94af4f352d81a2316c228a07c
stone725 <inf33554432@gmail.com>
2019-09-16 10:16:19 +0900

    https://atcoder.jp/contests/arc050/tasks/arc050_a

---
+++ AtCoder/arc050/arc050_a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char C, c;
+    cin >> C >> c;
+    if(C - 'A' == c - 'a'){
+        cout << "Yes\n";
+    }else{
+        cout << "No\n";
+    }
+}
