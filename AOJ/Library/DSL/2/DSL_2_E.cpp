f94ce1de8421e6eb64e72aec7f0d72d9683ed041
stone725 <inf33554432@gmail.com>
2019-09-28 19:21:20 +0900

    AOJ DSL_2_E

---
+++ AOJ/Library/DSL/2/DSL_2_E.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <algorithm>
+
+struct RangeAddQuery{
+    int n;
+    std::vector<int> dat;
+
+    void init(int n_){
+        n = 1;
+        while(n < n_) n *= 2;
+        dat.resize(2 * n - 1, 0);
+    }
+
+    void add(int a, int b, int k, int l, int r, int x){
+        if(r <= a || b <= l) return;
+        if(a <= l && r <= b){
+            dat[k] += x;
+        }
+        else{
+            add(a, b, k * 2 + 1, l, (l + r) / 2, x);
+            add(a, b, k * 2 + 2, (l + r) / 2, r, x);
+        }
+    }
+
+    void add(int a, int b, int x){
+        add(a, b, 0, 0, n, x);
+    }
+
+    int query(int k){
+        k += n - 1;
+        int res = dat[k];
+        while(k > 0) {
+            k = (k - 1) / 2;
+            res += dat[k];
+        }
+        return res;
+    }
+};
+
+using namespace std;
+
+int main(){
+    int n, q;
+    cin >> n >> q;
+    RangeAddQuery raq;
+    raq.init(n);
+    while(q--){
+        int com;
+        cin >> com;
+        if(com){
+            int i;
+            cin >> i;
+            cout << raq.query(i - 1) << "\n";
+        }else{
+            int s, t, x;
+            cin >> s >> t >> x;
+            raq.add(s - 1, t, x);
+        }
+    }
+}
