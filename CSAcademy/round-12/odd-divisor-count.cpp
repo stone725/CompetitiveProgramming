d24ca9649ab78b8a0b98a36186a6f30c3634a7c1
stone725 <inf33554432@gmail.com>
2019-09-29 00:25:47 +0900

    odd-divisor-count

---
+++ CSAcademy/round-12/odd-divisor-count.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    int ans = 0;
+    for(int i = a; i <= b; i++){
+        int cnt = 0;
+        for(int j = 1; j * j <= i; j++){
+            if(i % j) continue;
+            cnt += 1 + (j * j < i);
+        }
+        ans += cnt % 2;
+    }
+    cout << ans << "\n";
+}
