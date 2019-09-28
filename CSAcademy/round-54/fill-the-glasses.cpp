a30597f1a398fc5fd1f677e86a3f2d1ae8e5cda4
stone725 <inf33554432@gmail.com>
2019-09-29 00:19:40 +0900

    CSAcademy. fill-the-glasses

---
+++ CSAcademy/round-54/fill-the-glasses.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    sort(begin(a), end(a));
+    cout << accumulate(begin(a), begin(a) + k, 99) / 100 << "\n";
+}
