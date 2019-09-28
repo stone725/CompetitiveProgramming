ab8d499da19544c2aacda470bd00525743e26435
stone725 <inf33554432@gmail.com>
2019-09-28 21:56:05 +0900

    AOJ 0358

---
+++ AOJ/PCK/Prelim/0358.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int m, f, b;
+    cin >> m >> f >> b;
+    if(m + f < b){
+        cout << "NA\n";
+    }else{
+        cout << max(0, b - m) << "\n";
+    }
+}
