4b8c3c4b28b3b39b413507d2f69f4b4e9aceb6af
stone725 <inf33554432@gmail.com>
2019-09-28 09:04:42 +0900

    AOJ DSL_3_C

---
+++ AOJ/Library/DSL/3/DSL_3_C.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    while(q--){
+        long long x;
+        cin >> x;
+        long long s = 0, g = 0, score = 0;
+        long long ans = 0;
+        while(g < n){
+            score += a[g];
+            while(x < score){
+                ans += g - s;
+                score -= a[s];
+                s++;
+            }
+            g++;
+        }
+        while(s < g){
+            ans += g - s;
+            s++;
+        }
+        cout << ans << "\n";
+    }
+}
