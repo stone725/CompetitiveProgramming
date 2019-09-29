4e1332b644e32f6476e05c57687866ad92da3a43
stone725 <inf33554432@gmail.com>
2019-09-29 09:09:29 +0900

    https://atcoder.jp/contests/abc142/tasks/abc142_a

---
+++ AtCoder/abc142/abc142_a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    if(n % 2 == 0){
+        cout << 0.5 << "\n";
+    }else{
+        cout << (1. * (n / 2 + 1)) / n << "\n";
+    }
+}
