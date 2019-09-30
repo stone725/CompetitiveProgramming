339fc4729796c670e4cdca4e2d48ec2515e611ae
stone725 <inf33554432@gmail.com>
2019-09-30 11:31:31 +0900

    cpsco2019_s1_a

---
+++ AtCoder/cpsco2019-s1/cpsco2019_s1_a.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, a;
+    cin >> n >> a;
+    cout << a / 3 + !!(a % 3) << " " << min(a, n / 3) << "\n";
+}
