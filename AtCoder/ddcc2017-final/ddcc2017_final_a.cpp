34e3d24db001f8bc72dc1cd195300b8c5adc7f8d
stone725 <inf33554432@gmail.com>
2019-10-01 15:22:46 +0900

    ddcc2017_final_a

---
+++ AtCoder/ddcc2017-final/ddcc2017_final_a.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <complex>
+
+using namespace std;
+
+int main(){
+    int k;
+    cin >> k;
+    const complex<double> p200(100, 100), p300(150, 150);
+    int ans200 = 0, ans300 = 0;
+    for(int i = 0; i < 300; i += k){
+        for(int j = 0; j < 300; j += k){
+            if(abs(p200 - complex<double>(i, j)) <= 100 && abs(p200 - complex<double>(i + k, j + k)) <= 100 && abs(p200 - complex<double>(i + k, j)) <= 100 && abs(p200 - complex<double>(i, j + k)) <= 100){
+                ans200++;
+            }
+            if(abs(p300 - complex<double>(i, j)) <= 150 && abs(p300 - complex<double>(i + k, j + k)) <= 150 && abs(p300 - complex<double>(i + k, j)) <= 150 && abs(p300 - complex<double>(i, j + k)) <= 150){
+                ans300++;
+            }
+        }
+    }
+    cout << ans200 << " " << ans300 << "\n";
+}
