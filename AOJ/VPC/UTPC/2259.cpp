6fa4a889ac628ba34b925de96731cb81993832e8
stone725 <inf33554432@gmail.com>
2019-10-04 05:52:30 +0900

    AOJ 2259

---
+++ AOJ/VPC/UTPC/2259.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int m, n;
+    cin >> m >> n;
+    int ans = 0;
+    for(int i = 0; i < m; i++){
+        int cnt = 0;
+        for(int j = 0; j < n; j++){
+            int a;
+            cin >> a;
+            cnt += a;
+        }
+        ans = max(ans, cnt);
+    }
+    cout << ans << "\n";
+}
