73f2fcf1e0aeba65e41a79911170da398de2c5ae
stone725 <inf33554432@gmail.com>
2019-10-03 15:27:11 +0900

    https://atcoder.jp/contests/agc038/tasks/agc038_a

---
+++ AtCoder/agc038/agc038_a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int h, w, a, b;
+    cin >> h >> w >> a >> b;
+    for(int i = 0; i < h; i++){
+        for(int j = 0; j < w; j++){
+            cout << (j < a == i < b);
+        }
+        cout << "\n";
+    }
+}
