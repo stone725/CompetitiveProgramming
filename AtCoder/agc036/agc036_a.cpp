82ee70e197741b57c0144a4325f4b84e034c42aa
stone725 <inf33554432@gmail.com>
2019-10-03 12:42:51 +0900

    https://atcoder.jp/contests/agc036/tasks/agc036_a

---
+++ AtCoder/agc036/agc036_a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long long s;
+    cin >> s;
+    if(s == 1e18){
+        cout << "0 0 0 1000000000 1000000000 0";
+        return 0;
+    }
+    cout << 0 << " " << 0 << " " << static_cast<long long>(1e9) << " " << 1 << " " << static_cast<long long>(1e9 - s % static_cast<long long>(1e9)) << " " << s / static_cast<long long>(1e9) + 1 << "\n";
+}
