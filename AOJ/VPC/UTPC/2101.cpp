f871c12e8e2d46206311530975a015ac10d024f4
stone725 <inf33554432@gmail.com>
2019-10-04 05:59:39 +0900

    AOJ 2101

---
+++ AOJ/VPC/UTPC/2101.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    while(cin >> n && n){
+            int score = 0;
+        for(int i = 1; i * i <= n && i < n; i++){
+            if(n % i) continue;
+            score += i;
+            if(i * i < n && i != 1){
+                score += n / i;
+            }
+        }
+        if(n == score){
+            cout << "perfect number\n";
+        }else if(n > score){
+            cout << "deficient number\n";
+        }else{
+            cout << "abundant number\n";
+        }
+    }
+}
