6e4a5e7a6fe12d9c9278c65838f931753fa195a4
stone725 <inf33554432@gmail.com>
2019-10-05 14:16:18 +0900

    AOJ 2216

---
+++ AOJ/VPC/KUPC/2216.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    while(cin >> a >> b && a && b){
+        b -= a;
+        cout << (b % 500) / 100 << " " << (b % 1000) / 500 << " " << b / 1000 << "\n"; 
+    }
+}
