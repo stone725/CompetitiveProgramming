dd2e19f8fb46334b4ecdcb8dacff961703c72fd3
stone725 <inf33554432@gmail.com>
2019-10-06 19:13:10 +0900

    AOJ ALDS1_1_C

---
+++ AOJ/Lesson/ALDS1/ALDS1_1_C.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<bool> isPrime(10001, true);
+vector<int> primeNumbers;
+
+int main(){
+    isPrime[0] = isPrime[1] = false;
+    for(int i = 2; i * i <= 10000; i += 2){
+        if(!isPrime[i]) continue;
+        for(int j = i * i; j <= 10000; j += i){
+            isPrime[j] = false;
+        }
+        i -= i == 2;
+    }
+    int n, ans = 0;
+    for(int i = 0; i <= 10000; i++){
+        if(isPrime[i]){
+            primeNumbers.push_back(i);
+        }
+    }
+    cin >> n;
+    while(n--){
+        int a;
+        cin >> a;
+        if(a <= 1e4){
+            ans += isPrime[a];
+            continue;
+        }
+        ans++;
+        for(int i = 0; i < primeNumbers.size() && primeNumbers[i] * primeNumbers[i] <= a; i++){
+            if(a % primeNumbers[i] == 0){
+                ans--;
+                break;
+            }
+        }
+    }
+    cout << ans << "\n";
+}
