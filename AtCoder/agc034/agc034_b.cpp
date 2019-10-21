78dd3030939c996063fffc60dd6136122bffecf0
stone725 <inf33554432@gmail.com>
2019-10-21 09:38:12 +0900

    https://atcoder.jp/contests/agc034/tasks/agc034_b

---
+++ AtCoder/agc034/agc034_b.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    string s2;
+    for(int i = 0; i + 1 < s.size(); i++){
+        if(s.substr(i, 2) == "BC"){
+            i++;
+            s2 += "D";
+        }else{
+            s2 += s[i];
+        }
+    }
+    long long ans = 0, a = 0;
+    for(int i = 0; i < s2.size(); i++){
+        if(s2[i] == 'B' || s2[i] == 'C'){
+            a = 0;
+        }else if(s2[i] == 'D'){
+            if(a){
+                ans += a;
+            }
+        }else{
+            a++;
+        }
+    }
+    cout << ans << "\n";
+}
