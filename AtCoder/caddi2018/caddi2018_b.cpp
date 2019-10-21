0f89e0a2ff282c2726acf9e42a3dc5e7ed723361
stone725 <inf33554432@gmail.com>
2019-10-21 17:58:23 +0900

    https://atcoder.jp/contests/caddi2018/tasks/caddi2018_b

---
+++ AtCoder/caddi2018/caddi2018_b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    bool check = false;
+    for(int i = 0; i < n; i++){
+        int a;
+        cin >> a;
+        check = check || (a % 2);
+    }
+    if(check){
+        cout << "first\n";
+    }else{
+        cout << "second\n";
+    }
+}
