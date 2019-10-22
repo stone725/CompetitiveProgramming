55f81e86e4ecf82ccb24f1c9ec85d58430bbcf48
stone725 <inf33554432@gmail.com>
2019-10-22 22:35:14 +0900

    https://atcoder.jp/contests/abc003/tasks/abc003_3

---
+++ AtCoder/abc003/abc003_3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    vector<double> a(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    sort(rbegin(a), rend(a));
+    double ans = 0;
+    sort(begin(a), begin(a) + k);
+    for(int i = 0; i < k; i++){
+        ans = (ans + a[i]) / 2;
+    }
+    cout << setprecision(10) << ans << "\n";
+}
