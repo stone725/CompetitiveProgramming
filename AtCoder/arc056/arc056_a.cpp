c1bc0f0efe069d9e72e07d1a444a57e54233e9d1
stone725 <inf33554432@gmail.com>
2019-10-26 22:15:15 +0900

    https://atcoder.jp/contests/arc056/tasks/arc056_a

---
+++ AtCoder/arc056/arc056_a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    long long a, b, k, l;
+    cin >> a >> b >> k >> l;
+    long long ans = b * (k / l);
+    if(k % l){
+        ans += min((k % l) * a, b);
+    }
+    cout << ans << "\n";
+}
