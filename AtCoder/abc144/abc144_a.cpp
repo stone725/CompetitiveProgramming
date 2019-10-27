b43a510bcc60361cb0a6eef35380eaf5313c3d87
stone725 <inf33554432@gmail.com>
2019-10-28 01:02:23 +0900

    https://atcoder.jp/contests/abc144/tasks/abc144_a

---
+++ AtCoder/abc144/abc144_a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    if(9 < a || 9 < b){
+        cout << "-1\n";
+    }else{
+        cout << a * b << "\n";
+    }
+}
