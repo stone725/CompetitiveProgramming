64e3b0deb66272331804a14502fc69085e2eb165
stone725 <inf33554432@gmail.com>
2019-10-28 01:10:36 +0900

    https://atcoder.jp/contests/abc144/tasks/abc144_c

---
+++ AtCoder/abc144/abc144_c.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    long long n;
+    cin >> n;
+    long long ans = n - 1;
+    for(long long i = 1; i * i <= n; i++){
+        if(n % i) continue;
+        ans = min(ans, i + n / i - 2);
+    }
+    cout << ans << "\n";
+}
