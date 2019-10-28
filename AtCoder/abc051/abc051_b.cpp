3bcab8c814e35a174f84a5bd483bc01a14b2a564
stone725 <inf33554432@gmail.com>
2019-10-28 09:56:55 +0900

    https://atcoder.jp/contests/abc051/tasks/abc051_b

---
+++ AtCoder/abc051/abc051_b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int k, s;
+    cin >> k >> s;
+    int ans = 0;
+    for(int x = 0; x <= k; x++){
+        for(int y = 0; y <= k; y++){
+            if(s < x + y) break;
+            ans += s - x - y <= k;
+        }
+    }
+    cout << ans << "\n";
+}
