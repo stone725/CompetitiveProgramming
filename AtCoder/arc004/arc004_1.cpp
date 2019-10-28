021341d2345457efd760a72edf2769ee5324253b
stone725 <inf33554432@gmail.com>
2019-10-28 09:53:23 +0900

    https://atcoder.jp/contests/arc004/tasks/arc004_1

---
+++ AtCoder/arc004/arc004_1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <complex>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<complex<double>> c(n);
+    for(auto&& i : c){
+        int x, y;
+        cin >> x >> y;
+        i = complex<double>(x, y);
+    }
+    double ans = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            ans = max(ans, abs(c[i] - c[j]));
+        }
+    }
+    cout << ans << "\n";
+}
