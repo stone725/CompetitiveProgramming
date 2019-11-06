546ed805155ba4403802da82454068536172c696
stone725 <inf33554432@gmail.com>
2019-11-06 16:21:34 +0900

    https://atcoder.jp/contests/arc016/tasks/arc016_2

---
+++ AtCoder/arc016/arc016_2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string before = ".........";
+    int cnt = 0;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        string str;
+        cin >> str;
+        for(int j = 0; j < str.size(); j++){
+            if(str[j] == 'x' || (str[j] == 'o' && before[j] != 'o')){
+                cnt++;
+            }
+        }
+        before = str;
+    }
+    cout << cnt << "\n";
+}
