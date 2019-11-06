bb7511fbc944853f3ec0975be28dbe12ca419f9c
stone725 <inf33554432@gmail.com>
2019-11-06 09:58:30 +0900

    https://atcoder.jp/contests/arc014/tasks/arc014_2

---
+++ AtCoder/arc014/arc014_2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    string nowstring;
+    cin >> nowstring;
+    set<string> s;
+    s.insert(nowstring);
+    bool win = true;
+    for(int i = 0; i < n - 1; i++){
+        string nextstring;
+        cin >> nextstring;
+        if(nextstring[0] != nowstring.back() || s.count(nextstring)){
+            if(win){
+                cout << "WIN\n";
+                return 0;
+            }else{
+                cout << "LOSE\n";
+                return 0;
+            }
+        }
+        win = !win;
+        nowstring = nextstring;
+        s.insert(nowstring);
+    }
+    cout << "DRAW\n";
+}
