ce5c485661148a48d87e0ddc8594141900310488
stone725 <inf33554432@gmail.com>
2019-11-07 17:11:04 +0900

    https://atcoder.jp/contests/abc028/tasks/abc028_d

---
+++ AtCoder/abc028/abc028_d.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    double ans = 1. / n * (k - 1) / n * (n - k) / n * 3 * 2;
+    ans += 1. / n / n * (n - k) / n * 3;
+    ans += 1. / n / n * (k - 1) / n * 3;
+    ans += 1. / n / n / n;
+    cout << fixed << setprecision(10) << ans << "\n";
+}
