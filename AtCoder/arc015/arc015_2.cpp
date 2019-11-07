9d197426c1c925f68599eb5e5f4764ed2f924b6e
stone725 <inf33554432@gmail.com>
2019-11-07 12:17:41 +0900

    https://atcoder.jp/contests/arc015/tasks/arc015_2

---
+++ AtCoder/arc015/arc015_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int cnt[6] = {};
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        double high, low;
+        cin >> high >> low;
+        if(high < 0){
+            cnt[5]++;
+        }else if(low < 0){
+            cnt[4]++;
+        }
+        cnt[3] += 25 <= low;
+        if(35 <= high){
+            cnt[0]++;
+        }else if(30 <= high){
+            cnt[1]++;
+        }else if(25 <= high){
+            cnt[2]++;
+        }
+    }
+    cout << cnt[0];
+    for(int i = 1; i < 6; i++){
+        cout << " " << cnt[i];
+    }
+    cout << "\n";
+}
