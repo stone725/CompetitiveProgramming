62630dfb6c39d55d2db1fac824cf5e1c05fa9a63
stone725 <inf33554432@gmail.com>
2019-11-11 08:56:52 +0900

    tkppc4_2_a

---
+++ AtCoder/tkppc4-2/tkppc4_2_a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+    if(y < 0 || y % 2 == 1 || abs(x) > y / 2 || (y / 2) % 2 != abs(x) % 2){
+        cout << "-1\n";
+        return 0;
+    }
+    cout << y / 2 << "\n";
+}
