39b553bb05ee972b44f842b89b050e05e99460e4
stone725 <inf33554432@gmail.com>
2019-11-10 15:58:11 +0900

    https://atcoder.jp/contests/arc023/tasks/arc023_2

---
+++ AtCoder/arc023/arc023_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int r, c, d;
+    cin >> r >> c >> d;
+    int ans = 0;
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            int num;
+            cin >> num;
+            if((i + j) <= d && (i + j) % 2 == d % 2){
+                ans = max(ans, num);
+            }
+        }
+    }
+    cout << ans << "\n";
+}
