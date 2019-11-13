af19689a73a300107ce0554de9a22444439b6ccb
stone725 <inf33554432@gmail.com>
2019-11-13 12:55:17 +0900

    utpc2011_1

---
+++ AtCoder/utpc2011/utpc2011_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, m, ans = 0;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        int cnt = 0;
+        for(int j = 0; j < m; j++){
+            int solve;
+            cin >> solve;
+            cnt += solve;
+        }
+        ans = max(ans, cnt);
+    }
+    cout << ans << "\n";
+}
