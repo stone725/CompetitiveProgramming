9a049f23f87ac87e30c5b48a6782d58fe2a98345
stone725 <inf33554432@gmail.com>
2019-11-13 13:58:39 +0900

    https://atcoder.jp/contests/abc030/tasks/abc030_c

---
+++ AtCoder/abc030/abc030_c.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    int x, y;
+    cin >> x >> y;
+    vector<int> a(n), b(m);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    for(auto&& i : b){
+        cin >> i;
+    }
+    int t = 0, ans = 0;
+    while(lower_bound(begin(a), end(a), t) != end(a)){
+        auto p = lower_bound(begin(b), end(b), *lower_bound(begin(a), end(a), t) + x);
+        if(p == end(b)){
+            break;
+        }
+        t = *p + y;
+        ans++;
+    }
+    cout << ans << "\n";
+}
