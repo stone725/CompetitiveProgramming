154715ddf93f98d88a23cc351b020f6ec3d7db61
stone725 <inf33554432@gmail.com>
2019-11-14 12:35:34 +0900

    https://atcoder.jp/contests/arc052/tasks/arc052_a

---
+++ AtCoder/arc052/arc052_a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    int ans = 0;
+    for(auto&& i : str){
+        if('0' <= i && i <= '9'){
+            ans *= 10;
+            ans += i - '0';
+        }
+    }
+    cout << ans << "\n";
+}
