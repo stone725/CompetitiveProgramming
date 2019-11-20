9ac82461df9c48243ce748f512d35b50b3f56138
stone725 <inf33554432@gmail.com>
2019-11-20 13:50:33 +0900

    https://atcoder.jp/contests/utpc2013/tasks/utpc2013_01

---
+++ AtCoder/utpc2013/utpc2013_01.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    int cnt[4] = {};
+    bool check = true;
+    for(int i = 0; i < 4; i++){
+        switch(str[i]){
+        case 'B':
+            cnt[i] += 2;
+            break;
+        case 'A':
+        case 'D':
+        case 'O':
+        case 'P':
+        case 'Q':
+        case 'R':
+            cnt[i]++;
+            break;
+        }
+        switch (i)
+        {
+        case 2:
+            check &= cnt[i] == 1;
+            break;
+        default:
+            check &= cnt[i] == 0;
+        }
+    }
+    if(check){
+        cout << "yes\n";
+    }else{
+        cout << "no\n";
+    }
+}
