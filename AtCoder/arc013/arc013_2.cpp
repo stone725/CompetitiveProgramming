e4bfb1ba43557bd053f72765150a4e96c5115b39
stone725 <inf33554432@gmail.com>
2019-11-20 12:49:19 +0900

    https://atcoder.jp/contests/arc013/tasks/arc013_2

---
+++ AtCoder/arc013/arc013_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <array>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main(){
+    int c;
+    cin >> c;
+    array<int, 3> item;
+    array<int, 3> ans{0, 0, 0};
+    for(int i = 0; i < c; i++){
+        for(auto&& j : item){
+            cin >> j;
+        }
+        sort(rbegin(item), rend(item));
+        for(int j = 0; j < 3; j++){
+            ans[j] = max(ans[j], item[j]);
+        }
+    }
+    cout << accumulate(begin(ans), end(ans), 1ll, multiplies<long long>()) << "\n";
+}
