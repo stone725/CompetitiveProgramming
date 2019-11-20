94165518c25f6365689216a5cd8e30ca9e2d6b96
stone725 <inf33554432@gmail.com>
2019-11-20 13:36:53 +0900

    https://atcoder.jp/contests/abc004/tasks/abc004_3

---
+++ AtCoder/abc004/abc004_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    n %= 30;
+    string str = "123456";
+    for(int i = 0; i < n; i++){
+        swap(str[i % 5], str[i % 5 + 1]);
+    }
+    cout << str << "\n";
+}
