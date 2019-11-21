23cb699ee83ebc21e8d78a53c44b65193ec10b6a
stone725 <inf33554432@gmail.com>
2019-11-21 14:58:42 +0900

    https://atcoder.jp/contests/arc012/tasks/arc012_2

---
+++ AtCoder/arc012/arc012_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    long double va, vb, l;
+    cin >> n >> va >> vb >> l;
+    for(int i = 0; i < n; i++){
+        l /= va;
+        l *= vb;
+    }
+    cout << fixed << l << "\n";
+}
