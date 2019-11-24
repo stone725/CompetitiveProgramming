93250509f04892286c552c54ccfa13b832385d83
stone725 <inf33554432@gmail.com>
2019-11-24 09:42:32 +0900

    ddcc2020_qual_a

---
+++ AtCoder/ddcc2020-qual/ddcc2020_qual_a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+    int ans = 0;
+    switch(x){
+    case 1:
+        ans += 100000;
+    case 2:
+        ans += 100000;
+    case 3:
+        ans += 100000;
+    }
+    switch(y){
+    case 1:
+        ans += 100000;
+    case 2:
+        ans += 100000;
+    case 3:
+        ans += 100000;
+    }
+    ans += 400000 * (x * y == 1);
+    cout << ans << "\n";
+}
