5658f9d0634a45308dccb3fff64599231836bba0
stone725 <inf33554432@gmail.com>
2019-11-25 13:41:41 +0900

    https://atcoder.jp/contests/abc146/tasks/abc146_b

---
+++ AtCoder/abc146/abc146_b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+    for(auto&& i : str){
+        cout << static_cast<char>((i - 'A' + n) % 26 + 'A');
+    }
+    cout << "\n";
+}
