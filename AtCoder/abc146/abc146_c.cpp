7dbd7f990883f50515b859382d2cce28d8f757a2
stone725 <inf33554432@gmail.com>
2019-11-25 13:49:18 +0900

    https://atcoder.jp/contests/abc146/tasks/abc146_c

---
+++ AtCoder/abc146/abc146_c.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+long long a, b, x;
+
+long long calc(int n){
+    return a * n + b * to_string(n).size();
+}
+
+int main(){
+    cin >> a >> b >> x;
+    int s = 0, g = 1e9 + 1;
+    while(g - s > 1){
+        int mid = (s + g) / 2;
+        if(calc(mid) <= x){
+            s = mid;
+        }else{
+            g = mid;
+        }
+    }
+    cout << s << "\n";
+}
