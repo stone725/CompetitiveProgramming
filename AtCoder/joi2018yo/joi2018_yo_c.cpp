11ec46955cf0760a02273f3c72adc376476010f8
stone725 <inf33554432@gmail.com>
2019-11-28 15:55:49 +0900

    joi2018_yo_c

---
+++ AtCoder/joi2018yo/joi2018_yo_c.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int h, w;
+    cin >> h >> w;
+    vector<vector<int>> a(h, vector<int>(w));
+    for(auto&& i : a){
+        for(auto&& j : i){
+            cin >> j;
+        }
+    }
+    int ans = 1e9 + 7;
+    for(int i = 0; i < h; i++){
+        for(int j = 0; j < w; j++){
+            int cnt = 0;
+            for(int k = 0; k < h; k++){
+                for(int l = 0; l < w; l++){
+                    cnt += min(abs(k - i), abs(l - j)) * a[k][l];
+                }
+            }
+            ans = min(ans, cnt);
+        }
+    }
+    cout << ans << "\n";
+}
