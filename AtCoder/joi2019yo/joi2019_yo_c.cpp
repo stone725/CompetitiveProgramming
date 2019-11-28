0a3ea8924b97ce8dec1352c31e14075833dd9bed
stone725 <inf33554432@gmail.com>
2019-11-28 15:59:26 +0900

    joi2019_yo_c

---
+++ AtCoder/joi2019yo/joi2019_yo_c.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    string str;
+    cin >> str;
+    int ans = 0;
+    for(int i = 0; i + 1 < n; i++){
+        if(str[i] != str[i + 1]){
+            i++;
+            ans++;
+        }
+    }
+    cout << ans << "\n";
+}
