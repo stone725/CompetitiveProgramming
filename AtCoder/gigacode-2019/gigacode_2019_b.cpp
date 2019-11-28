b3a11750ffbf1934b983fb212a2d84baeb85e704
stone725 <inf33554432@gmail.com>
2019-11-28 12:45:29 +0900

    gigacode_2019_b

---
+++ AtCoder/gigacode-2019/gigacode_2019_b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, x, y, z;
+    cin >> n >> x >> y >> z;
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        int a, b;
+        cin >> a >> b;
+        ans += x <= a && y <= b && z <= a + b;
+    }
+    cout << ans << "\n";
+}
