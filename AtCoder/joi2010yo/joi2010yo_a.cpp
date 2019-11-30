f4e036402418d8e64c60f6c7f39ccf4866c24f24
stone725 <inf33554432@gmail.com>
2019-11-30 21:08:06 +0900

    joi2010yo_a

---
+++ AtCoder/joi2010yo/joi2010yo_a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int sum;
+    cin >> sum;
+    for(int i = 0; i < 9; i++){
+        int m;
+        cin >> m;
+        sum -= m;
+    }
+    cout << sum << "\n";
+}
