3049d9ca73362b2830fccd0f09ffab80b9c9b1c6
stone725 <inf33554432@gmail.com>
2019-11-30 21:20:37 +0900

    https://yukicoder.me/problems/no/932

---
+++ yukicoder/932.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    for(int i = 0; i < str.size(); i += 3){
+        if(str.size() < i + 2 || str.substr(i, 2) != "AC"){
+            cout << "Failed...\n";
+            return 0;
+        }
+    }
+    cout << "Done!\n";
+}
