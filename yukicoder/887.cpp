cc9c4e53fac2588e06bbea0252276fca49b9f5a7
stone725 <inf33554432@gmail.com>
2019-12-01 08:40:38 +0900

    https://yukicoder.me/problems/no/887

---
+++ yukicoder/887.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, nmax;
+    cin >> n;
+    nmax = n;
+    int i = 0;
+    while(n != 1){
+        if(n % 2){
+            n = 3 * n + 1;
+        }else{
+            n /= 2;
+        }
+        nmax = max(nmax, n);
+        i++;
+    }
+    cout << i << "\n" << nmax << "\n";
+}
