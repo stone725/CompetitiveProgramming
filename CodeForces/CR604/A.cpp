2153f622247d575e6f9f6e03713942db96cf51f6
stone725 <inf33554432@gmail.com>
2019-12-06 08:08:44 +0900

    https://codeforces.com/contest/1265/problem/A

---
+++ CodeForces/CR604/A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    while(n--){
+        string str;
+        cin >> str;
+        str = "D" + str + "\n";
+        bool flag = false;
+        for(int i = 1; i + 1 < str.size(); i++){
+            if(str[i] != '?' && str[i] == str[i + 1]){
+                flag = true;
+                break;
+            }
+            if(str[i] == '?'){
+                for(char c = 'a'; c < 'd'; c++){
+                    if(c != str[i - 1] && c != str[i + 1]){
+                        str[i] = c;
+                        break;
+                    }
+                }
+            }
+        }
+        if(flag){
+            cout << "-1\n";
+            continue;
+        }
+        cout << str.substr(1);
+    }
+}
