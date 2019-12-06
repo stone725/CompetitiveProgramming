b074d0863970aa2398eef68935067c876f8a7033
stone725 <inf33554432@gmail.com>
2019-12-06 15:32:48 +0900

    https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/6/ITP1_6_B

---
+++ AOJ/Lesson/ITP1/6/ITP1_6_B.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <map>
+#include <set>
+#
+
+using namespace std;
+
+int main(){
+    int n;
+    char c[] = {'S','H','C','D'};
+    cin >> n;
+    map<char, set<int>> m;
+    for(auto&& i : c){
+        for(int j = 1; j <= 13; j++){
+            m[i].insert(j);
+        }
+    }
+    for(int i = 0; i < n; i++){
+        char mark;
+        do{
+            cin >> mark;
+        }while(mark < 'A' || 'Z' < mark);
+        int num;
+        cin >> num;
+        m[mark].erase(num);
+    }
+    for(auto&& i : c){
+        for(auto&& j : m[i]){
+            cout << i << " " << j << "\n";
+        }
+    }
+}
