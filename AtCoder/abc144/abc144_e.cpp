017314b43ce3f2bca68d6b74ba0fa2a0355df9e1
stone725 <inf33554432@gmail.com>
2019-12-09 10:18:04 +0900

    https://atcoder.jp/contests/abc144/tasks/abc144_e

---
+++ AtCoder/abc144/abc144_e.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long n, k;
+vector<long long> a(n), b(n);
+
+bool check(long long x){
+    long long num = 0;
+    for(int i = 0; i < n; i++){
+        long long c = x / b[i];
+        num += max<long long>(0, a[i] - c);
+    }
+    return num <= k;
+}
+
+int main(){
+    cin >> n >> k;
+    a.resize(n);
+    b.resize(n);
+    for(auto&& i : a){
+        cin >> i;
+    }
+    for(auto&& i : b){
+        cin >> i;
+    }
+    sort(begin(a), end(a));
+    sort(rbegin(b), rend(b));
+    long long s = -1, g = 1e12;
+    while(g - s > 1){
+        long long mid = (s + g) / 2;
+        if(check(mid)){
+            g = mid;
+        }else{
+            s = mid;
+        }
+    }
+    cout << g << "\n";
+}
