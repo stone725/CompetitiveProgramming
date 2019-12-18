062f8fe160168a3b14bb93e3a09dc8b58104fc50
stone725 <inf33554432@gmail.com>
2019-12-18 12:45:06 +0900

    codefestival_2015_qualA_b

---
+++ AtCoder/code-festival-2015-quala/codefestival_2015_qualA_b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    long long ans = 0;
+    for(int i = 0; i < n; i++){
+        int num;
+        cin >> num;
+        ans *= 2;
+        ans += num;
+    } 
+    cout << ans << "\n";
+}
