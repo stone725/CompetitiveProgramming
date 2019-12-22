277f8452d72a4fdf3530ed07bee71edabb3b37de
stone725 <inf33554432@gmail.com>
2019-12-22 11:36:18 +0900

    https://onlinejudge.u-aizu.ac.jp/challenges/sources/PCK/Prelim/0029?year=2004

---
+++ AOJ/PCK/Prelim/2004/0029.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main(){
+    string str, mostfind = "", mostlong = "";
+    map<string, int> many;
+    while(cin >> str){
+        if(many[mostfind] < ++many[str]){
+            mostfind = str;
+        }
+        if(mostlong.size() < many.size()){
+            mostlong = str;
+        }
+    }
+    cout << mostfind << " " << mostlong << "\n";
+}
