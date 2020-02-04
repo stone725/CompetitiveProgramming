ffeecc74d2abd61ce447f003703107197a845d8d
stone725 <inf33554432@gmail.com>
2020-02-04 11:29:18 +0900

    ALDS1_1_Dのフォーマット調整

--- AOJ/Lesson/ALDS1/1/ALDS1_1_D.cpp
+++ AOJ/Lesson/ALDS1/1/ALDS1_1_D.cpp
@@ -3,13 +3,15 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cin >> n;
     int min_doller;
     cin >> min_doller;
     int ans = -2 * 1e9;
-    for(int i = 1; i < n; i++){
+    for (int i = 1; i < n; i++)
+    {
         int doller;
         cin >> doller;
         ans = max(ans, doller - min_doller);
