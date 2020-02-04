65b9c36f2798dc1fad55c612f0ed484368ecde8e
stone725 <inf33554432@gmail.com>
2020-02-04 11:24:20 +0900

    https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/1/ALDS1_1_D

---
+++ AOJ/Lesson/ALDS1/1/ALDS1_1_D.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int min_doller;
+    cin >> min_doller;
+    int ans = -2 * 1e9;
+    for(int i = 1; i < n; i++){
+        int doller;
+        cin >> doller;
+        ans = max(ans, doller - min_doller);
+        min_doller = min(min_doller, doller);
+    }
+    cout << ans << "\n";
+}
