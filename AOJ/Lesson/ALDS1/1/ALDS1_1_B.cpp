1b1abbcab3e221dbf87700036fd6a10af6b67ada
stone725 <inf33554432@gmail.com>
2020-02-04 11:36:44 +0900

    https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/1/ALDS1_1_B

---
+++ AOJ/Lesson/ALDS1/1/ALDS1_1_B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    return b ? gcd(b, a % b) : a;
+}
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << gcd(x, y) << "\n";
+}
