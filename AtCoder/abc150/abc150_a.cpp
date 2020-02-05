1f25e76ae1f9d0678023bf28ff7e5fa691207711
stone725 <inf33554432@gmail.com>
2020-02-05 20:19:06 +0900

    https://atcoder.jp/contests/abc150/tasks/abc150_a

---
+++ AtCoder/abc150/abc150_a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k, x;
+    cin >> k >> x;
+    if (x <= k * 500)
+    {
+        cout << "Yes\n";
+    }
+    else
+    {
+        cout << "No\n";
+    }
+}
