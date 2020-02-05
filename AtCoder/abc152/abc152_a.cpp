02d9eb22f4722e5fcd9e7a6d80165313f6b21ca5
stone725 <inf33554432@gmail.com>
2020-02-05 20:14:18 +0900

    https://atcoder.jp/contests/abc152/tasks/abc152_a

---
+++ AtCoder/abc152/abc152_a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    if (n == m)
+    {
+        cout << "Yes\n";
+    }
+    else
+    {
+        cout << "No\n";
+    }
+}
