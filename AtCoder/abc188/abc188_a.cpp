7898365c182fed1be6c9a43acb8e412f67e8343d
stone725 <inf33554432@gmail.com>
2021-02-13 15:17:25 +0900

    https://atcoder.jp/contests/abc188/submissions/20136354

---
+++ AtCoder/abc188/abc188_a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (max(a, b) - min(a, b) < 3)
+    {
+        cout << "Yes\n";
+    }
+    else
+    {
+        cout << "No\n";
+    }
+}
